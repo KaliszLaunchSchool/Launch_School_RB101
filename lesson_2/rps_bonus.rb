62e437a0c359f07d8fa0dc64a7764e073fa127be
Martha Kalisz <mskalisz3@gmail.com>
2021-03-28 21:20:38 +0000

    Refactor: Add lizard/spock to the game

---
+++ lesson_2/rps_bonus.rb
@@ -0,0 +1,64 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
+
+def test_method
+  prompt('test message')
+end
+
+# test_method
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+test_method
+
+def win?(first, second)
+  (first == 'scissors' && second == 'paper') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'rock' && second == 'lizard')
+    (first == 'lizard' && second == 'spock') ||
+    (first == 'spock' && second == 'scissors') ||
+    (first == 'scissors' && second == 'lizard') ||
+    (first == 'lizard' && second == 'paper') ||
+    (first == 'paper' && second == 'spock') ||
+    (first == 'spock' && second == 'rock') ||
+    (first == 'rock' && second == 'scissors') 
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do
+  # player's turn
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  # computer's turn
+  computer_choice = VALID_CHOICES.sample
+
+  Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for playing. Goodbye!")
