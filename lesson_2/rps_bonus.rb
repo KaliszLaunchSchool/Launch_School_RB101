d29e319fdb31e136974950e6282a47886112883e
mskalisz <mskalisz3@gmail.com>
2021-04-07 13:06:26 -0500

    Create loop for grand winner

--- lesson_2/rps_bonus.rb
+++ lesson_2/rps_bonus.rb
@@ -44,15 +44,13 @@ def get_results(player, computer)
 end
 
 def calculate_match_score(score, winner)
-  p winner
-  p score[winner.to_sym]
   score[winner.to_sym] += 1 unless winner.nil?
 end
 
 def display_match_results(score)
-  prompt("Current score")
-  prompt("Player: #{score['player'.to_sym]}")
-  prompt("Computer: #{score['computer'.to_sym]}")
+  prompt("  Current score")
+  prompt("  Player: #{score['player'.to_sym]}")
+  prompt("  Computer: #{score['computer'.to_sym]}")
 end
 
 def play_again?
@@ -71,39 +69,46 @@ end
 prompt ("Welcome to the 'Rock, Paper, Scissors, Lizard, Spock' Game!")
 score = {'player': 0, 'computer': 0}
 loop do
-  prompt ("The first player to 5 wins, wins the match!")
-  # player's turn
-  player_choice = ''
   loop do
-    
-    prompt("Choose one: ")
-    VALID_CHOICES.each do |word, shorthand|
-      puts "   #{word} (#{shorthand})"
+    prompt ("The first player to 5 wins, wins the match!")
+    # player's turn
+    player_choice = ''
+    winner = ''
+    loop do
+
+      prompt("Choose one: ")
+      VALID_CHOICES.each do |word, shorthand|
+        puts "   #{word} (#{shorthand})"
+      end
+
+      player_choice = Kernel.gets().chomp().downcase
+
+      if VALID_CHOICES.include?(player_choice)
+        break
+      elsif VALID_CHOICES.values.include?(player_choice)
+        player_choice = VALID_CHOICES.key(player_choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
     end
+
+    # computer's turn
+    computer_choice = VALID_CHOICES.keys.sample
+
+    Kernel.puts("You chose: #{player_choice}; Computer chose: #{computer_choice}")
+
+    display_results(player_choice, computer_choice)
+    winner =  get_results(player_choice, computer_choice)
+
+    calculate_match_score(score, winner)
+    display_match_results(score)
     
-    player_choice = Kernel.gets().chomp().downcase
-    
-    if VALID_CHOICES.include?(player_choice)
-      break
-    elsif VALID_CHOICES.values.include?(player_choice)
-      player_choice = VALID_CHOICES.key(player_choice)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
+    grand_winner = score.value?(5)
+    break if grand_winner
   end
-
-  # computer's turn
-  computer_choice = VALID_CHOICES.keys.sample
-
-  Kernel.puts("You chose: #{player_choice}; Computer chose: #{computer_choice}")
-
-  display_results(player_choice, computer_choice)
-  winner =  get_results(player_choice, computer_choice)
-
-  calculate_match_score(score, winner)
-  display_match_results(score)
-
+  grand_winner = score.key(5).to_s.capitalize
+  prompt("#{grand_winner} is the grand winner!")
   break unless play_again?
   clear_screen
 end
