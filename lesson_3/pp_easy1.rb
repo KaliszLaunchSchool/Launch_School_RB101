5182327daabe251d0ce96c55eec5cd6028ca05b1
mskalisz <mskalisz3@gmail.com>
2021-04-08 10:40:34 -0500

    Solve q8

--- lesson_3/pp_easy1.rb
+++ lesson_3/pp_easy1.rb
@@ -113,7 +113,7 @@ or
 =begin
 Question 7
 If we build an array like this:
-=end
+
 flintstones = ["Fred", "Wilma"]
 flintstones << ["Barney", "Betty"]
 flintstones << ["BamBam", "Pebbles"]
@@ -121,11 +121,13 @@ flintstones << ["BamBam", "Pebbles"]
 
 #["Fred", "Wilma", ["Barney", "Betty"], ["BamBam", "Pebbles"]]
 #Make this into an un-nested array.
-flintstones.flatten
+flintstones.flatten!
+=end
 =begin
 Question 8
 Given the hash below
 
 flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
-Turn this into an array containing only two elements: Barney's name and Barney's number
-=end
+#Turn this into an array containing only two elements: Barney's name and Barney's number
+ flintstones.assoc("Barney")
+ =end
