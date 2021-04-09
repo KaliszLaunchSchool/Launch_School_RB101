f2a8feea987583a08cc41b379231d35226e7b1e7
mskalisz <mskalisz3@gmail.com>
2021-04-09 16:06:26 -0500

    Solve q3

--- lesson_3/pp_medium1.rb
+++ lesson_3/pp_medium1.rb
@@ -30,6 +30,11 @@ Question 3
 Alan wrote the following method, which was intended to show all of the 
 factors of the input number:
 
+Alyssa noticed that this will fail if the input is 0, or a negative number, 
+and asked Alan to change the loop. How can you make this work without using 
+begin/end/until? Note that we're not looking to find the factors for 0 or 
+negative numbers, but we just want to handle it gracefully instead of 
+raising an exception or going into an infinite loop.
 def factors(number)
   divisor = number
   factors = []
@@ -39,15 +44,44 @@ def factors(number)
   end until divisor == 0
   factors
 end
-Alyssa noticed that this will fail if the input is 0, or a negative number, and asked Alan to change the loop. How can you make this work without using begin/end/until? Note that we're not looking to find the factors for 0 or negative numbers, but we just want to handle it gracefully instead of raising an exception or going into an infinite loop.
+
+def factors(number)
+  divisor = number
+  factors = []
+  if number <= 0
+    puts "Please enter a number greater than 0"
+  else
+    begin
+      factors << number / divisor if number % divisor == 0
+      divisor -= 1
+    end until divisor == 0
+  end
+  factors
+end
+
+p factors(-5)
+
+#LS solution
+while divisor > 0 do
+  factors << number / divisor if number % divisor == 0
+  divisor -= 1
+end
 
 Bonus 1
 What is the purpose of the number % divisor == 0 ?
-
+to determine what numbers divide equally into the number
+LS Solution: Allows you to determine if the result of the division 
+is an integer number (no remainder).
 Bonus 2
-What is the purpose of the second-to-last line (line 8) in the method (the factors before the method's end)?
-
-Solution 3
+What is the purpose of the second-to-last line (line 8) in the method (the 
+factors before the method's end)?
+To return the factors
+LS Solution: This is what is the actual return value from the method. Recall that 
+without an explicit return statement in the code, the return value of the method 
+is the last statement executed. If we omitted this line, the code would execute, 
+but the return value of the method would be nil.
+=end
+=begin
 Question 4
 Alyssa was asked to write an implementation of a rolling buffer. Elements are added to the rolling buffer and if the buffer becomes full, then new elements that are added will displace the oldest elements in the buffer.
 
