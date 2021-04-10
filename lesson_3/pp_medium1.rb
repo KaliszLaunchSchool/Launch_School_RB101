7ecc3896c1f0687cf4a326200e633e49812f385a
mskalisz <mskalisz3@gmail.com>
2021-04-10 08:22:34 -0500

    Solve q5

--- lesson_3/pp_medium1.rb
+++ lesson_3/pp_medium1.rb
@@ -114,11 +114,12 @@ argument.
 =end
 =begin
 Question 5
-Alyssa asked Ben to write up a basic implementation of a Fibonacci calculator. A user passes in two numbers, and the calculator will keep computing the sequence until some limit is reached.
+Alyssa asked Ben to write up a basic implementation of a Fibonacci calculator. 
+A user passes in two numbers, and the calculator will keep computing the sequence 
+until some limit is reached.
 
-Ben coded up this implementation but complained that as soon as he ran it, he got an error. Something about the limit variable. What's wrong with the code?
-
-Copy Code
+Ben coded up this implementation but complained that as soon as he ran it, he got 
+an error. Something about the limit variable. What's wrong with the code?
 limit = 15
 
 def fib(first_num, second_num)
@@ -132,9 +133,26 @@ end
 
 result = fib(0, 1)
 puts "result is #{result}"
+
+The error is undefiened local variable 'limit' and is because limit is not being
+passed into the method
+=end
+limit = 15
+
+def fib(first_num, second_num, limit)
+  while first_num + second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(0, 1, limit)
+puts "result is #{result}"
+=begin
 How would you fix this so that it works?
 
-Solution 5
 Question 6
 What is the output of the following code?
 
