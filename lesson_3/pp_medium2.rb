539cb7bb7a66353c780f4a69e159013faf909cc3
mskalisz <mskalisz3@gmail.com>
2021-04-10 20:57:23 -0500

    Solve q5

--- lesson_3/pp_medium2.rb
+++ lesson_3/pp_medium2.rb
@@ -354,10 +354,36 @@ tricky_method(my_string, my_array)
 
 puts "My string looks like this now: #{my_string}"
 puts "My array looks like this now: #{my_array}"
-Whether the above "coincidentally" does what we think we wanted "depends" upon what is going on inside the method.
+Whether the above "coincidentally" does what we think we wanted "depends" upon what 
+is going on inside the method.
 
-How can we change this code to make the result easier to predict and easier for the next programmer to maintain?
+How can we change this code to make the result easier to predict and easier for the 
+next programmer to maintain?
+We can change the code to both use the << operator
 
+LS solution
+def not_so_tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param += ["rutabaga"]
+
+  return a_string_param, an_array_param
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+my_string, my_array = not_so_tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
+Every language (Ruby included) provides ways and means of writing "clever" code that 
+depends on some of the less obvious traits of the language.
+
+Every good programmer practices these tricks...and then avoids them like the plague.
+
+Clever programmers don't write "clever" code. They write explicit code that is easy 
+to read, debug and modify as the requirements change.
+=end
+=begin
 Question 6
 How could the unnecessary duplication in this method be removed?
 
