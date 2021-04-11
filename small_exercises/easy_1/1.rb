dc888740d74bdc76a5e29bff5f7567a993315c09
mskalisz <mskalisz3@gmail.com>
2021-04-10 22:01:41 -0500

    Add prompt

---
+++ small_exercises/easy_1/1.rb
@@ -0,0 +1,22 @@
+=begin
+Write a method that takes two arguments, a string and a positive integer, and prints 
+the string as many times as the integer indicates.
+
+Example:
+
+repeat('Hello', 3)
+Output:
+
+Hello
+Hello
+Hello
+
+P
+Create a method with 2 arguments(string, integer)
+print the string so many times
+E
+D
+A
+C
+=end
+
