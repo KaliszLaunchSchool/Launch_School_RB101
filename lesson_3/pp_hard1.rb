9425fda26ffa7765ef69cdafbdda630514c007f4
mskalisz <mskalisz3@gmail.com>
2021-04-10 21:08:47 -0500

    Solve q2

--- lesson_3/pp_hard1.rb
+++ lesson_3/pp_hard1.rb
@@ -27,7 +27,25 @@ informal_greeting << ' there'
 
 puts informal_greeting  #  => "hi there"
 puts greetings
-Solution 2
+
+# greetings should still be the hash { a: 'hi' } as it is reassigned to 
+# informal_greeting prior to its mutation
+WRONG
+LS Solution
+The output is {:a=>"hi there"}. The reason is because informal_greeting is a 
+reference to the original object. The line informal_greeting << ' there' was using 
+the String#<< method, which modifies the object that called it. This means that the 
+original object was changed, thereby impacting the value in greetings. If instead of 
+modifying the original object, we wanted to only modify informal_greeting but not 
+greetings, there are a couple of options:
+
+we could initialize informal_greeting with a reference to a new object containing 
+the same value by informal_greeting = greetings[:a].clone.
+
+we can use string concatenation, informal_greeting = informal_greeting + ' there', 
+which returns a new String object instead of modifying the original object.
+=end
+=begin
 Question 3
 In other practice problems, we have looked at how the scope of variables affects the modification of one "layer" when they are passed to another.
 
