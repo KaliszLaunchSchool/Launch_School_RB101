6d780af6e1b30ece90e41ef1fd9110c313174ef6
mskalisz <mskalisz3@gmail.com>
2021-04-10 21:55:42 -0500

    Add prompt for each exercise

---
+++ small_exercises/documentation/1.rb
@@ -0,0 +1,4 @@
+=begin
+Locate the ruby documentation for methods File::path and File#path. 
+How are they different?
+=end
---
+++ small_exercises/documentation/2.rb
@@ -0,0 +1,10 @@
+# Assume you have the following code:
+
+require 'date'
+
+puts Date.civil
+puts Date.civil(2016)
+puts Date.civil(2016, 5)
+puts Date.civil(2016, 5, 13)
+
+# What will each of the 4 puts statements print?
