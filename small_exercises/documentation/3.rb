c4c9a99f45b3903d597b5d65177d0fff1c54523f
mskalisz <mskalisz3@gmail.com>
2021-04-11 18:38:14 -0400

    Add LS solution to Default Arguments in the middle

--- small_exercises/documentation/3.rb
+++ small_exercises/documentation/3.rb
@@ -12,3 +12,27 @@ my_method(4, 5, 6)
 # https://ruby-doc.org/core-2.4.0/doc/syntax/calling_methods_rdoc.html
 # 4 is assigned to a, 6 is assigned to d, 5 needs a value so it is assigned to b(overrides default), 
 # c remains its default value 
+=begin
+LS solution
+Approach/Algorithm
+We are using "default positional arguments" here.
+
+Solution
+[4, 5, 3, 6]
+
+Discussion
+Information about arguments can be found at http://ruby-doc.org/ by selecting the 
+doc/syntax/calling_methods.rdoc link from the core API page. If you scroll down to the Default Positional 
+Arguments section and start reading, you will learn that ruby lets you use default arguments in the middle 
+of a number of positional arguments, much as we are doing here. The documentation shows that positional 
+arguments are filled out first, then the default arguments are set with any values supplied, and, finally, 
+default values are applied to anything remaining.
+
+In this exercise, 4, 5 and 6 are assigned to the positional arguments a, b and d. Note that 5 overrides the 
+default value for b, but c is not set to any of the listed values. c, instead, is assigned its default value, 
+3. Thus, when the results are printed, we see the results shown in our solution.
+
+Don't worry too much if you don't understand exactly how this works. This exercise is mostly concerned with 
+where to find this information should you run into a case where it is difficult to determine how the arguments 
+to a method are interpreted. More importantly, try not to do this in your own code; it's hard to understand.
+=end
