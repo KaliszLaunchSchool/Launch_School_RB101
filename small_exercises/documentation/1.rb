e3784e1da6bca457eabab0b7dcbcbbf5281dcdf0
mskalisz <mskalisz3@gmail.com>
2021-04-11 17:45:32 -0400

    Add LS solution for  class & instance method

--- small_exercises/documentation/1.rb
+++ small_exercises/documentation/1.rb
@@ -19,4 +19,26 @@ File#Path
 
   File.new("testfile").path               #=> "testfile"
   File.new("/tmp/../tmp/xxx", "w").path   #=> "/tmp/../tmp/xxx"
+
+LS Solution
+Both methods can be found on the documentation page for the File class in the 
+Core API section. File::path is a class method, while File#path is an instance 
+method.
+
+Discussion
+Class methods are called on the class, while instance methods are called on objects. 
+Thus:
+
+puts File.path('bin')
+calls the class method File::path since we're calling it on the File class, 
+while:
+
+f = File.new('my-file.txt')
+puts f.path
+calls the instance method File#path since we're calling it on an object of the 
+File class, namely f.
+
+Pay attention when reading the documentation; make sure you are using a class 
+method when you need a class method, and an instance method when you need an 
+instance method.
 =end
