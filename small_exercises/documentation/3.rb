005e87535c554106dcf52a8900871c7e1eabe4cd
mskalisz <mskalisz3@gmail.com>
2021-04-11 18:35:23 -0400

    Solve default arguments in the middle

--- small_exercises/documentation/3.rb
+++ small_exercises/documentation/3.rb
@@ -6,5 +6,9 @@ end
 
 my_method(4, 5, 6)
 
-# Use the ruby documentation to determine what this code will print.
 
+# Use the ruby documentation to determine what this code will print.
+# => [4, 5, 3, 6]
+# https://ruby-doc.org/core-2.4.0/doc/syntax/calling_methods_rdoc.html
+# 4 is assigned to a, 6 is assigned to d, 5 needs a value so it is assigned to b(overrides default), 
+# c remains its default value 
