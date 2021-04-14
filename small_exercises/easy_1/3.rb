c7517a4e66198884c91c9a2b2d268d1f567602a8
mskalisz <mskalisz3@gmail.com>
2021-04-14 15:17:39 -0400

    Complete PED of PEDAC for exercise 3

--- small_exercises/easy_1/3.rb
+++ small_exercises/easy_1/3.rb
@@ -3,17 +3,33 @@ Write a method that takes one argument, a positive integer, and returns a list o
 
 Examples:
 
-Copy Code
 puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
 puts digit_list(7) == [7]                     # => true
 puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
 puts digit_list(444) == [4, 4, 4]             # => true
 
 ## Problem ##
+- Write a method
+- Argument is a positive integer
+- Prints the list of digits in the number
+
+- What about zero? Looks like 0 is acceptable
+
 
 ## Examples ##
+Input: positive integer
+Output: Array
+Return: boolean
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
 
 ## Data ##
+integer
+array
+boolean
 
 ## Algorithhm ##
 
