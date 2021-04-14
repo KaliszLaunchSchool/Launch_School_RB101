6c443b5d3fd753d6db2a368e751fcc85163adf65
mskalisz <mskalisz3@gmail.com>
2021-04-14 15:09:08 -0400

    Add LS solution exercise 2, 'odd'

--- small_exercises/easy_1/2.rb
+++ small_exercises/easy_1/2.rb
@@ -61,3 +61,31 @@ puts is_odd?(-17)  # => true
 puts is_odd?(-8)   # => false
 puts is_odd?(0)    # => false
 puts is_odd?(7)    # => true
+
+=begin
+LS Solution
+
+def is_odd?(number)
+  number % 2 == 1
+end
+
+Discussion
+To determine if a number is odd without using #odd? or #even?, we must check whether the number modulo 2 is 1. 
+In Ruby, we use % to perform modulo operations, so we use it here to determine whether the number is odd.
+
+Further Exploration
+This solution relies on the fact that % is the modulo operator in Ruby, not a remainder operator as in some 
+other languages. Remainder operators return negative results if the number on the left is negative, while 
+modulo always returns a non-negative result if the number on the right is positive.
+
+modulo	remainder
+5 mod 2 == 1	5 rem 2 == 1
+-5 mod 2 == 1	-5 rem 2 == -1
+5 mod -2 == -1	5 rem -2 == 1
+-5 mod -2 == -1	-5 rem -2 == -1
+If you weren't certain whether % were the modulo or remainder operator, how would you rewrite #is_odd? 
+so it worked regardless?
+
+The Integer#remainder method performs a remainder operation in Ruby. Rewrite #is_odd? to use Integer#remainder
+instead of %. Note: before version 2.4, Ruby used the Numeric#remainder method instead.
+=end
