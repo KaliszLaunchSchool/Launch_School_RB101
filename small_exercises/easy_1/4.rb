5158bf67def6c8fbedd5fea62b6cc4555ad8e43c
mskalisz <mskalisz3@gmail.com>
2021-04-15 08:53:58 -0400

    Initial PEDAC reasoning for exercise 4

--- small_exercises/easy_1/4.rb
+++ small_exercises/easy_1/4.rb
@@ -8,20 +8,44 @@ vehicles = [
 
 count_occurrences(vehicles)
 
-The words in the array are case-sensitive: 'suv' != 'SUV'. ` Once counted, print each element alongside the 
-number of occurrences.
+The words in the array are case-sensitive: 'suv' != 'SUV'. ` Once counted, print each element 
+alongside the number of occurrences.
 
 Expected output:
 
-Copy Code
 car => 4
 truck => 3
 SUV => 1
 motorcycle => 2
 
 ## Problem ##
+- Create a method
+- Given an array of elements
+- Count the number of elements in an array
+- Print each element along side the number of occurences
+- Case matters 'suv' != 'SUV'
+
+Input: Array
+Output: Hash
+
 ## Example ##
+vehicles = [
+  'car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'motorcycle', 'car', 'truck'
+]
+
+Expected output:
+car => 4
+truck => 3
+SUV => 1
+motorcycle => 2
+
 ## Data ##
+Array
+Strings(or other elements?) within the array
+Integer
+Hash
+
 # Algorithm ##
 ## Code ##
 =end
