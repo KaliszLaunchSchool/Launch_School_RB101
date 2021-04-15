544f9bb43771b384bd8863519dbea2cad5c52253
mskalisz <mskalisz3@gmail.com>
2021-04-15 19:52:31 -0400

    Add LS solution exercise 5

--- small_exercises/easy_1/5.rb
+++ small_exercises/easy_1/5.rb
@@ -46,3 +46,21 @@ end
 puts reverse_sentence('') == ''
 puts reverse_sentence('Hello World') == 'World Hello'
 puts reverse_sentence('Reverse these words') == 'words these Reverse'
+
+=begin
+LS Solution
+
+def reverse_sentence(string)
+  string.split.reverse.join(' ')
+end
+
+Discussion
+To reverse the order of substrings within a string, we first need to separate those substrings. In our 
+solution, we use #split with no arguments to separate each word and place it in an array. To reverse the order 
+of the words, we then invoke #reverse on the array. Now, string looks like this:
+
+'Hello World'.split.reverse # => ["World", "Hello"]
+
+That's precisely what we want except we need it to be a string, not an array. To accomplish this, we invoke 
+#join which joins every element in an array using the given argument as the delimiter.
+=end
