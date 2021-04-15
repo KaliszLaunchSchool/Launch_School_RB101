155222744365bd57436c48d966ddac648bcde521
mskalisz <mskalisz3@gmail.com>
2021-04-15 10:05:54 -0400

    Copy prompt exercise 5, initiate PEDAC process

---
+++ small_exercises/easy_1/5.rb
@@ -0,0 +1,29 @@
+=begin
+Write a method that takes one argument, a string, and returns a new string with the words in reverse order.
+
+Examples:
+
+puts reverse_sentence('') == ''
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
+The tests above should print true.
+
+## Problem ##
+- Create a method that takes a string
+- Returns a new string in reverse order
+
+## Examples ##
+- Do cases get changed? No
+- Do the letters get reversed as well? no, just the words
+
+puts reverse_sentence('') == ''
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
+The tests above should print true.
+
+## Data ##
+String
+
+## Algorithm ##
+## Code ##
+=end
