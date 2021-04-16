d7d9e82beeaaf9ba3bbe19805c5543e3fa89f449
mskalisz <mskalisz3@gmail.com>
2021-04-15 20:14:53 -0400

    Solve exercise 6 Reverse It Part 2

--- small_exercises/easy_1/6.rb
+++ small_exercises/easy_1/6.rb
@@ -14,9 +14,16 @@ puts reverse_words('Launch School')         # => hcnuaL loohcS
   - If 5 or more characters, reverse the order of the letters
   - If less than 5 characters, string remains in the right order
   - Spaces remain
+  - Words stay in the same order
 - Returns string 
 
+Input: String
+Output: String
+
+Capital letters remain
+
 ## Examples ##
+
 puts reverse_words('Professional')          # => lanoisseforP
 puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
 puts reverse_words('Launch School')         # => hcnuaL loohcS
@@ -26,6 +33,27 @@ String
 Array?
 
 ## Algorithm ##
+- Create method 
+- Split each string (creates array)
+- Iterate through array, check for 5 or more letters
+- If 5 or more letters, reverse string
+- Join array
+- Return string
 
 ## Code ##
 =end
+def reverse_words(string)
+  each_word = string.split
+  reversed_each_word = each_word.map { |word|
+    if word.size >= 5 then
+      word.reverse
+    else
+      word
+    end
+  }
+  reversed_each_word.join(' ')
+end
+
+puts reverse_words('Professional')          # => lanoisseforP
+puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
+puts reverse_words('Launch School')         # => hcnuaL loohcS
