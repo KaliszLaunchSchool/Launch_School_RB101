6eb3f018c8d96ee50e0f68f9ec3cbcb76ec70fc7
mskalisz <mskalisz3@gmail.com>
2021-04-15 20:19:43 -0400

    Add LS solution exercise 6 Reverse It Part 2

--- small_exercises/easy_1/6.rb
+++ small_exercises/easy_1/6.rb
@@ -57,3 +57,33 @@ end
 puts reverse_words('Professional')          # => lanoisseforP
 puts reverse_words('Walk around the block') # => Walk dnuora the kcolb
 puts reverse_words('Launch School')         # => hcnuaL loohcS
+
+=begin
+LS Solution
+
+def reverse_words(string)
+  words = []
+
+  string.split.each do |word|
+    word.reverse! if word.size >= 5
+    words << word
+  end
+
+  words.join(' ')
+end
+
+Discussion
+When given a string or an array, and asked to evaluate each character or element, your first instinct should 
+be to reach for an iterator. Our goal here is to iterate over the given string and check each word for the 
+number of characters it contains. If it has five or more characters then we'll reverse the word.
+
+At the top of our method, we create an empty array named words that will hold each modified word of the result:
+these words will be reversed if long, or as-is if they are short. We use #each to iterate over string, but 
+first, we need to separate each word in string using #split, which returns an array containing the separated 
+words. For each word, we count the number of characters it contains using #size. If it contains five or more 
+characters, we use the destructive method #reverse! to reverse the word. We mutate word so that we can add it 
+to words by invoking words << word.
+
+After iterating over string and evaluating each word, words will contain all of the words, with longer words 
+reversed. Finally, we can invoke words.join(' ') to return the desired string.
+=end
