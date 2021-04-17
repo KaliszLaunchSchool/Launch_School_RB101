015cef522e7d323f1e351a30dc8ec35886eed15d
mskalisz <mskalisz3@gmail.com>
2021-04-17 16:44:36 -0400

    Solve exercise 2

---
+++ lesson_4/adtl_practice_probs/2.rb
@@ -0,0 +1,12 @@
+=begin
+Add up all of the ages from the Munster family hash:
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+=end
+
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+ages_ary = []
+
+ages.each_value {|num| ages_ary << num }  
+add_ages = ages_ary.sum
+p add_ages
