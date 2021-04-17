02f503251bbc51f167f529156881ebc9f3992957
mskalisz <mskalisz3@gmail.com>
2021-04-17 19:46:06 -0400

    Add LS solution exercise 5

--- lesson_4/adtl_practice_probs/5.rb
+++ lesson_4/adtl_practice_probs/5.rb
@@ -10,3 +10,9 @@ flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
 name = flintstones.bsearch { |name| name.start_with?('Be') }
 index = flintstones.index(name)
 p index
+
+=begin
+LS solution
+
+flintstones.index { |name| name[0, 2] == "Be" }
+=end
