ab39440b90d280d5d103939db58b77ef3f07b217
mskalisz <mskalisz3@gmail.com>
2021-04-17 19:44:34 -0400

    Solve exercise 5

---
+++ lesson_4/adtl_practice_probs/5.rb
@@ -0,0 +1,12 @@
+=begin
+In the array:
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+Find the index of the first name that starts with "Be"
+=end
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+name = flintstones.bsearch { |name| name.start_with?('Be') }
+index = flintstones.index(name)
+p index
