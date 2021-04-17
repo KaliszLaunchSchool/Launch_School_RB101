4558f71b309d43afee611948657d67b2061c9568
mskalisz <mskalisz3@gmail.com>
2021-04-17 11:48:14 -0400

    Initiate prompt for exercise 8, complete PED of PEDAC

---
+++ small_exercises/easy_1/8.rb
@@ -0,0 +1,39 @@
+=begin
+Array Average
+Write a method that takes one argument, an array containing integers, and returns the average of all numbers 
+in the array. The array will never be empty and the numbers will always be positive integers. Your result 
+should also be an integer.
+
+Examples:
+
+puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
+puts average([1, 5, 87, 45, 8, 8]) == 25
+puts average([9, 47, 23, 95, 16, 52]) == 40
+The tests above should print true.
+
+## Problem ##
+- Write a method
+- Take an array which contains integers
+- Find the average of all the numbers in the array
+- Rules
+ - Array will not be empty
+ - Integers always positive
+
+Input: array containing integers
+Output: integer
+
+Integer or floaat division? Integer!
+
+## Examples ##
+
+puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
+puts average([1, 5, 87, 45, 8, 8]) == 25
+puts average([9, 47, 23, 95, 16, 52]) == 40
+
+## Data ##
+Array
+Integer
+
+## Algorithm ##
+## Code ##
+=end
