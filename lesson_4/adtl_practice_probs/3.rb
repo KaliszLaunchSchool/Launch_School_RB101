a328fceec34b0f6b50fb64e7fea55ddbc8722ea0
mskalisz <mskalisz3@gmail.com>
2021-04-17 19:14:50 -0400

    Add LS solution exercise 3

--- lesson_4/adtl_practice_probs/3.rb
+++ lesson_4/adtl_practice_probs/3.rb
@@ -9,3 +9,13 @@ ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
 
 ages.delete_if { |key, value| value >= 100}
 p ages
+
+=begin
+LS Solution
+
+ages.keep_if { |_, age| age < 100 }
+
+You could also use #select! here. When using similar methods however, it is important to be aware of the 
+subtle differences in their implementation. For example, the Ruby Documentation for Hash#select! tells us that 
+it is "Equivalent to Hash#keep_if, but returns nil if no changes were made", though in this case that wouldn't 
+have made any difference.
