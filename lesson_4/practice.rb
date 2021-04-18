3e1d8ceda69829b9c5b1d60c855f79a0ea08ad7b
mskalisz <mskalisz3@gmail.com>
2021-04-18 12:52:32 -0400

    Practice with lesson_4

--- lesson_4/practice.rb
+++ lesson_4/practice.rb
@@ -1,3 +1,4 @@
+=begin
 loop do
   number = rand(1..10)   # a random number between 1 and 10
   puts 'Hello!'
@@ -5,4 +6,43 @@ loop do
     puts 'Exiting...'
     break
   end
+end
+
+def upper_snake(str)
+  words = str.split
+  current_word = 0
+
+  loop do
+    break if current_word == words.size
+    words[current_word].upcase!
+    current_word += 1
+  end
+
+  words.join('_')
+end
+
+sentence = 'The sky was blue'
+p upper_snake(sentence) # => 'THE_SKY_WAS_BLUE'
+
+{a: 1, b: 2, c: 3, d: 4, e: 5}.map do |_, num|
+  plus_one = num + 1
+  puts "#{num} plus 1 is #{plus_one}"
+end
+=end
+countries_and_capitals = {
+  'France' => 'Paris',
+  'Belgium' => 'Brussels',
+  'Morocco' => 'Rabat',
+  'Barbados' => 'Bridgetown',
+  'Peru' => 'Lima',
+  'Bolivia' => 'La Paz',
+  'Brazil' => 'Brasilia'
+}
+
+def begins_with_b(string)
+  string[0] == 'B'
+end
+
+p countries_and_capitals.find_all do |country, capital|
+  begins_with_b(country)
 end
