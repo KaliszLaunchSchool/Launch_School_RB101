74025186f574b967b3060d49138ed6765c7d0647
mskalisz <mskalisz3@gmail.com>
2021-04-21 08:15:49 -0400

    Solve exercise 9

--- small_exercises/easy_1/9.rb
+++ small_exercises/easy_1/9.rb
@@ -12,12 +12,39 @@ The tests above should print true.
 For a challenge, try writing this without any basic looping constructs (while, until, loop, and each).
 
 # Problem
+- Create a method with one argument 
+  - Argument is a positive integer
+- Sum the digits of that integer
+- Return the sum
 
 # Examples
+puts sum(23) == 5
+puts sum(496) == 19
+puts sum(123_456_789) == 45
 
 # Data
+- Integer
+- String
+- Array
 
 # Algo
+- Create a method which takes one argument 
+  - Argument is a positive integer
+  - Verify positive integer?
+- Convert integer to a string
+- Split the string
+- Take the sum of the array
+- Return the sum
 
 # Code
 =end
+
+def sum(integer)
+  each_number = integer.to_s.split(//)
+  each_integer = each_number.map {|num| num.to_i}
+  each_integer.sum
+end
+
+puts sum(23) == 5
+puts sum(496) == 19
+puts sum(123_456_789) == 45
