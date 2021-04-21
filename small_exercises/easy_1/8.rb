f4df5b5d1763656f583779bbffc3a442c6c56d90
mskalisz <mskalisz3@gmail.com>
2021-04-21 06:57:03 -0400

    Add LS solution exercise 8

--- small_exercises/easy_1/8.rb
+++ small_exercises/easy_1/8.rb
@@ -49,6 +49,7 @@ Integer
 ## Code ##
 =end
 
+=begin
 def average(array)
   divisor = array.length
   sum = array.sum
@@ -58,3 +59,29 @@ end
 puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
 puts average([1, 5, 87, 45, 8, 8]) == 25
 puts average([9, 47, 23, 95, 16, 52]) == 40
+=end
+
+=begin
+LS Solution
+
+def average(numbers)
+  sum = numbers.reduce { |sum, number| sum + number }
+  sum / numbers.count
+end
+
+Discussion
+Two things need to be done to find the average. First, add every number together. Second, divide the sum by 
+the number of elements. We accomplish the first part by using Enumerable#reduce(also known as #inject), which 
+combines all elements of the given array by applying a binary operation. This operation is specified by a 
+block or symbol. We used a block in our solution, but we could have just as easily used a symbol, like this:
+
+numbers.reduce(:+)
+
+Once we have the sum, all that's left is to divide it by the number of elements. To do that, we use #count to 
+count the number of elements in numbers. Then, we divide sum by the number of elements and return the quotient.
+
+Further Exploration
+Currently, the return value of average is an Integer. When dividing numbers, sometimes the quotient isn't a 
+whole number, therefore, it might make more sense to return a Float. Can you change the return value of 
+average from an Integer to a Float?
+=end
