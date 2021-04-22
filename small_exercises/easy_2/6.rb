f271e35b95f1de5b0640f5e8001eb095f2f9d956
mskalisz <mskalisz3@gmail.com>
2021-04-22 10:31:31 -0400

    Add LS solution exercise 6

--- small_exercises/easy_2/6.rb
+++ small_exercises/easy_2/6.rb
@@ -31,3 +31,23 @@ range = 1..99
 array = range.to_a
 
 array.each {|num| puts num if num.odd? }
+
+=begin
+LS solution
+Solution
+
+value = 1
+while value <= 99
+  puts value
+  value += 2
+end
+
+Discussion
+There are a variety of different ways to approach this problem, so don't be worried if your solution is significantly different.
+
+Our solution simply takes a very basic approach of counting up from 1 to 99 by 2s, and printing each value.
+
+Further Exploration
+Repeat this exercise with a technique different from the one you just used, and different from the solution shown above. You may want 
+to explore the use Integer#upto or Array#select methods, or maybe use Integer#odd?
+=end
