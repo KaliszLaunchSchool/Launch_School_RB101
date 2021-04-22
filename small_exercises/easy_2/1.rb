a3f92e45911838f6983659282c3c130cbf0a56d4
mskalisz <mskalisz3@gmail.com>
2021-04-21 20:47:33 -0400

    Add LS solution exercise 1

--- small_exercises/easy_2/1.rb
+++ small_exercises/easy_2/1.rb
@@ -37,3 +37,25 @@ age_range = (20..200).to_a
 teddys_age = age_range.sample
 
 p "Teddy is #{teddys_age} years old!"
+
+=begin
+Approach/Algorithm
+Hint: you may find the rand method useful. It is documented in the Kernel module.
+
+Solution
+
+age = rand(20..200)
+puts "Teddy is #{age} years old!"
+
+Discussion
+Our solution uses Kernel#rand with a range (20..200) as an argument. This use is described in the documentation 
+for rand, although it is kind of an afterthought.
+
+Nevertheless, the use of a range to limit the output value is an enormous help here, so we use it to generate 
+Teddy's age. Afterward, all we have to do is print the result.
+
+Further Exploration
+Modify this program to ask for a name, and then print the age for that person. For an extra challenge, use 
+"Teddy" as the name if no name is entered.
+=end
+
