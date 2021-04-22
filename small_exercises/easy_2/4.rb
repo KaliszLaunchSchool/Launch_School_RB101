aa961151a81c7ee3e274a2540936b2184449672f
mskalisz <mskalisz3@gmail.com>
2021-04-22 09:59:11 -0400

    Add LS solution exercise 4

--- small_exercises/easy_2/4.rb
+++ small_exercises/easy_2/4.rb
@@ -35,14 +35,36 @@ Integers
 
 # Code
 =end
-CURRENT_YEAR = 2021
+current_year = Time.now.year
+
 puts "What is your age?"
 user_age = gets.chomp.to_i
 puts "At what age would you like to retire?"
 retirement_age = gets.chomp.to_i
 
 years_until_retirement = retirement_age - user_age
-retirement_year = CURRENT_YEAR + years_until_retirement
+retirement_year = current_year + years_until_retirement
 
-puts "It is #{CURRENT_YEAR}. You'll retire in #{retirement_year}"
+puts "It is #{current_year}. You'll retire in #{retirement_year}"
 puts "You have only #{years_until_retirement} years of work to go!"
+
+=begin
+LS Solution
+
+print 'What is your age? '
+current_age = gets.to_i
+
+print 'At what age would you like to retire? '
+retirement_age = gets.to_i
+
+current_year = Time.now.year
+work_years_to_go = retirement_age - current_age
+retirement_year = current_year + work_years_to_go
+
+puts "It's #{current_year}. You will retire in #{retirement_year}. "
+puts  "You have only #{work_years_to_go} years of work to go!"
+
+Discussion
+In this solution, we use Time.now to get the current date. This returns a Time object. The Time class has a fairly useful method, 
+Time#year that gives us the current year. From there we can determine the retirement year based on the two inputs and the current year.
+=end
