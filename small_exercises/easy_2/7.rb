fcd58ee80f8aa83b16c8e9c4d2b1543195816a1c
mskalisz <mskalisz3@gmail.com>
2021-04-22 10:37:12 -0400

    Add LS solution exercise 7

--- small_exercises/easy_2/7.rb
+++ small_exercises/easy_2/7.rb
@@ -7,3 +7,16 @@ range = 1..99
 array = range.to_a
 
 array.each {|num| puts num if num.even? }
+
+=begin
+
+value = 1
+while value <= 99
+  puts value if value.even?
+  value += 1
+end
+
+Discussion
+Our solution is just a minor variation on the solution to print odd numbers. This time, though, we increment 
+the value by 1 with every iteration, and only print it if it is even.
+=end
