1b5ec84a6ed945554af94bdff71e490aad778a3e
mskalisz <mskalisz3@gmail.com>
2021-04-21 20:34:01 -0400

    Add LS Solution exercise 10

--- small_exercises/easy_1/10.rb
+++ small_exercises/easy_1/10.rb
@@ -46,3 +46,16 @@ end
 puts calculate_bonus(2800, true) == 1400
 puts calculate_bonus(1000, false) == 0
 puts calculate_bonus(50000, true) == 25000
+
+=begin
+LS Solution
+
+def calculate_bonus(salary, bonus)
+  bonus ? (salary / 2) : 0
+end
+
+Discussion
+The solution to this exercise takes advantage of the ternary operator. We're able to use bonus as the 
+condition because it's a boolean, which means its value will only be true or false. If it's true, then we 
+divide salary by 2 and return the quotient. If it's false, then we return 0.
+=end
