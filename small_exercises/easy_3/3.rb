b982fb79776f15d373072a95c970dc08a6e013b1
mskalisz <mskalisz3@gmail.com>
2021-04-23 08:25:47 -0400

    Add LS solution exercise 3

--- small_exercises/easy_3/3.rb
+++ small_exercises/easy_3/3.rb
@@ -59,3 +59,14 @@ characters = user_input_copy.chars.delete_if {|character| character == " "}
 character_count = characters.count
 
 puts "There are #{character_count} characters in \"#{user_input}\"."
+
+=begin
+print 'Please write word or multiple words: '
+input = gets.chomp
+number_of_characters = input.delete(' ').size
+puts "There are #{number_of_characters} characters in \"#{input}\"."
+
+Discussion
+We don't want to count spaces here, so we use String#delete to return a new String with all spaces removed 
+from it. The original string stored in input is left intact.
+=end
