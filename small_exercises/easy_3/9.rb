43881e1e905a4a91ce53f8c64d6171ed2e701e9a
mskalisz <mskalisz3@gmail.com>
2021-04-23 11:25:51 -0400

    Add LS solution exercise 9

--- small_exercises/easy_3/9.rb
+++ small_exercises/easy_3/9.rb
@@ -71,3 +71,26 @@ p real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
 p real_palindrome?('356653') == true
 p real_palindrome?('356a653') == true
 p real_palindrome?('123ab321') == false
+
+=begin
+LS Solution
+
+def real_palindrome?(string)
+  string = string.downcase.delete('^a-z0-9')
+  palindrome?(string)
+end
+
+Discussion
+Chances are you reached for a String#gsub here to eliminate the non-alphanumeric characters. There's nothing 
+wrong with that, but we'll take the opportunity to use and talk about String#delete instead. #delete is an 
+interesting method that takes arguments that sort of look like regular expressions, and then deletes 
+everything formed by the intersection of all its arguments. See the documentation for complete details.
+
+For our purposes, we need to remove the non-alphanumeric characters; to do that, we tell delete to delete 
+everything that isn't a letter or digit. We then pass the result to our original palindrome? method to 
+determine if the cleaned up string is a palindrome.
+
+Further Exploration
+Read the documentation for String#delete, and the closely related String#count (you need to read count to get 
+all of the information you need for delete.)
+=end
