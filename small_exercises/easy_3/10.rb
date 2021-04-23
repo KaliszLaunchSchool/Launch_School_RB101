f19ac18ac760e0039ed891b3d9e78abe86c27ff6
mskalisz <mskalisz3@gmail.com>
2021-04-23 11:33:53 -0400

    Add LS solution exercise 10

--- small_exercises/easy_3/10.rb
+++ small_exercises/easy_3/10.rb
@@ -19,3 +19,19 @@ p palindromic_number?(34543) == true
 p palindromic_number?(123210) == false
 p palindromic_number?(22) == true
 p palindromic_number?(5) == true
+
+=begin
+LS Solution
+def palindromic_number?(number)
+  palindrome?(number.to_s)
+end
+
+Discussion
+The hardest part of this program is recognizing that the easiest way to tell if a number is palindromic is to 
+convert it to a string first, then check whether that string is palindromic. To determine if the string is 
+palindromic, we use the palindrome? method we wrote earlier.
+
+Further Exploration
+Suppose your number begins with one or more 0s. Will your method still work? Why or why not? Is there any way 
+to address this?
+=end
