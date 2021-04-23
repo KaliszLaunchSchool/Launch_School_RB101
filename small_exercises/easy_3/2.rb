e7a848fcd74e0605bda281034a6105f0aaaa379c
mskalisz <mskalisz3@gmail.com>
2021-04-22 22:27:47 -0400

    Complete PEDA for exercise 2

---
+++ small_exercises/easy_3/2.rb
@@ -0,0 +1,51 @@
+=begin
+Arithmetic Integer
+Write a program that prompts the user for two positive integers, and then prints the results of the following 
+operations on those two numbers: addition, subtraction, product, quotient, remainder, and power. Do not worry 
+about validating the input.
+
+Example
+
+==> Enter the first number:
+23
+==> Enter the second number:
+17
+==> 23 + 17 = 40
+==> 23 - 17 = 6
+==> 23 * 17 = 391
+==> 23 / 17 = 1
+==> 23 % 17 = 6
+==> 23 ** 17 = 141050039560662968926103
+
+# Problem
+- Ask user for 2 positive integers
+- Print the results of each operaation on the 2 numbers:
+  - addition, subtraction, product, quotient, remainder, and power
+
+Input: Integer/Float
+Output: String? Integers?
+
+# Examples
+==> Enter the first number:
+23
+==> Enter the second number:
+17
+==> 23 + 17 = 40
+==> 23 - 17 = 6
+==> 23 * 17 = 391
+==> 23 / 17 = 1
+==> 23 % 17 = 6
+==> 23 ** 17 = 141050039560662968926103
+
+# Data 
+Float
+String? Integer?
+
+# Algo
+- Ask user for 2 positive integers
+- Store each in a variable
+- Perform each operation
+- Print each
+
+# Code
+=end
