0a87067b7d876f0603df8695020890e0f122b25c
mskalisz <mskalisz3@gmail.com>
2021-04-25 14:53:11 -0400

    Add LS solution exercise 3

--- small_exercises/easy_4/3.rb
+++ small_exercises/easy_4/3.rb
@@ -90,3 +90,49 @@ puts leap_year?(1700) == false
 puts leap_year?(1) == false
 puts leap_year?(100) == false
 puts leap_year?(400) == true
+
+=begin
+LS Solution
+
+def leap_year?(year)
+  if year % 400 == 0
+    true
+  elsif year % 100 == 0
+    false
+  else
+    year % 4 == 0
+  end
+end
+
+A shorter solution
+
+def leap_year?(year)
+  (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)
+end
+
+Discussion
+The first solution takes this one step at a time by testing for the least common cases first; years divisible 
+by 400, and years divisible by 100. If the year is anything else, then it is a leap year if it divisible by 4.
+
+The second solution is more idiomatic, but also a little harder to read. It is, in effect, identical to the 
+first solution, although the tests are turned around a little bit.
+
+Further Exploration
+The order in which you perform tests for a leap year calculation is important. For what years will leap_year? 
+fail if you rewrite it as:
+
+def leap_year?(year)
+  if year % 100 == 0
+    false
+  elsif year % 400 == 0
+    true
+  else
+    year % 4 == 0
+  end
+end
+
+Can you rewrite leap_year? to perform its tests in the opposite order of the above solution? That is, test 
+whether the year is divisible by 4 first, then, if necessary, test whether it is divisible by 100, and finally, 
+if necessary, test whether it is divisible by 400. Is this solution simpler or more complex than the original 
+solution?
+=end
