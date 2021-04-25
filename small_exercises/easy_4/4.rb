f368e936e706dec36518239a0c9a00ab6d491659
mskalisz <mskalisz3@gmail.com>
2021-04-25 14:58:47 -0400

    Add LS solution exercise 4

--- small_exercises/easy_4/4.rb
+++ small_exercises/easy_4/4.rb
@@ -50,3 +50,29 @@ puts leap_year?(1700) == true
 puts leap_year?(1) == false
 puts leap_year?(100) == true
 puts leap_year?(400) == true
+
+=begin
+LS Solution
+
+def leap_year?(year)
+  if year < 1752 && year % 4 == 0
+    true
+  elsif year % 400 == 0
+    true
+  elsif year % 100 == 0
+    false
+  else
+    year % 4 == 0
+  end
+end
+
+Discussion
+The change to the original leap_year? function is quite small; just one additional if clause at the beginning 
+of the method.
+
+Further Exploration
+Find a web page that talks about leap years or the different calendar systems, and talk about the interesting 
+information you learned. For instance, how was the change from the Julian calendar to the Gregorian calendar 
+handled in your ancestral lands? Do they even use these calendar systems? If you live someplace that doesn't 
+use the Gregorian calendar, tell us about your calendar system.
+=end
