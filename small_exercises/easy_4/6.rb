16a534dad5dbff4c006b68f6c78183d47c96cd6b
mskalisz <mskalisz3@gmail.com>
2021-04-25 19:57:41 -0400

    Add LS solution exercise 6

--- small_exercises/easy_4/6.rb
+++ small_exercises/easy_4/6.rb
@@ -54,3 +54,20 @@ p running_total([2, 5, 13]) == [2, 7, 20]
 p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
 p running_total([3]) == [3]
 p running_total([]) == []
+
+=begin
+LS Solution
+
+def running_total(array)
+  sum = 0
+  array.map { |value| sum += value }
+end
+
+Discussion
+This solution does nothing fancy; it just walks through the array calculating the running total while building 
+the resulting array. #map makes this really easy.
+
+Further Exploration
+Try solving this problem using Enumerable#each_with_object or Enumerable#inject (note that Enumerable methods 
+can be applied to Arrays).
+=end
