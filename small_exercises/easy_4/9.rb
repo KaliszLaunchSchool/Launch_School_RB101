44a7617745d2da91cfa13b909c6ef76718722765
mskalisz <mskalisz3@gmail.com>
2021-04-26 21:14:49 -0400

    Complete PED of PEDAC exercise 9

---
+++ small_exercises/easy_4/9.rb
@@ -0,0 +1,37 @@
+=begin
+Convert a Number to a String!
+In the previous two exercises, you developed methods that convert simple numeric strings to signed Integers. 
+In this exercise and the next, you're going to reverse those methods.
+
+Write a method that takes a positive integer or zero, and converts it to a string representation.
+
+You may not use any of the standard conversion methods available in Ruby, such as Integer#to_s, String(), 
+Kernel#format, etc. Your method should do this the old-fashioned way and construct the string by analyzing 
+and manipulating the number.
+
+Examples
+
+integer_to_string(4321) == '4321'
+integer_to_string(0) == '0'
+integer_to_string(5000) == '5000'
+
+# Problem
+- Create a method which takes a positive integer or zero and converts it to string
+- No to_s etc
+
+# Example
+integer_to_string(4321) == '4321'
+integer_to_string(0) == '0'
+integer_to_string(5000) == '5000'
+
+# Data
+Hash
+String
+Integer 
+Array
+
+# Algo
+- Create a hash to translate integer to string
+
+# Code
+=end
