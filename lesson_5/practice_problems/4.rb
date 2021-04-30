0151c11033958d0fc06a63851aa62d734574a84f
mskalisz <mskalisz3@gmail.com>
2021-04-30 09:51:23 -0400

    Add LS solution exercise 4

--- lesson_5/practice_problems/4.rb
+++ lesson_5/practice_problems/4.rb
@@ -18,3 +18,23 @@ p hsh1
 hsh2 = {['a'] => {a: ['1', :two, 3], b: 4}, 'b' => 5}
 hsh2[['a']][:a][2] = 4
 p hsh2
+
+=begin
+LS Solution
+
+arr1[1][1] = 4
+arr1 # => [1, [2, 4], 4]
+
+arr2[2] = 4
+arr2 # => [{:a=>1}, {:b=>2, :c=>[7, 6, 5], :d=>4}, 4]
+
+hsh1[:first][2][0] = 4
+hsh1 # => {:first=>[1, 2, [4]]}
+
+hsh2[['a']][:a][2] = 4
+hsh2 # => {["a"]=>{:a=>["1", :two, 4], :b=>4}, "b"=>5}
+
+In the last example the key for the first item in the outer hash is actually an array containing a single 
+string object ["a"]. Although this is unusual, it is important to remember that both hash keys and values can 
+be any Ruby object.
+=end
