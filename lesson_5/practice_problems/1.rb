12314eedf01c7ea9d1d567d2042c45d2c71a203e
mskalisz <mskalisz3@gmail.com>
2021-04-30 09:02:45 -0400

    Solve exercise 1

---
+++ lesson_5/practice_problems/1.rb
@@ -0,0 +1,33 @@
+=begin
+Practice Problem 1
+How would you order this array of number strings by descending numeric value?
+
+arr = ['10', '11', '9', '7', '8']
+
+# Problem
+Given an array or number strings
+Sort bu descending numeric value
+Input: Array
+Output: Sorted array
+
+# Example
+arr = ['10', '11', '9', '7', '8']
+    ==> ['11', '10', '9', '8', '7']
+
+# Data
+Array, strings, integers
+
+# Algo
+- Given an array of strings
+- Convert each string into integer
+- sort_by, descending
+- Convert each back to string
+- Return the sorted array
+=end
+
+arr = ['10', '11', '9', '7', '8']
+
+arr_of_i = arr.map { |string| string.to_i }
+arr_of_i.sort! { |a, b| b <=> a }
+arr = arr_of_i.map { |integer| integer.to_s }
+p arr
