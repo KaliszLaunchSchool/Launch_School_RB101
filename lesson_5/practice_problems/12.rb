e381a97e6e3a21328ef63f4c21b6dde17d47e038
mskalisz <mskalisz3@gmail.com>
2021-04-30 16:43:40 -0400

    Add LS solution exercise 12

--- lesson_5/practice_problems/12.rb
+++ lesson_5/practice_problems/12.rb
@@ -37,3 +37,18 @@ hash = {}
 arr.each do |element|
   p hash = element[0]=> element[1]
 end
+
+=begin
+LS Solution
+
+hsh = {}
+arr.each do |item|
+  hsh[item[0]] = item[1]
+end
+
+hsh # => {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
+
+Although this task may at first seem complicated because the collection contains different object types, sometimes 
+nested three levels deep, you only really have to work at the initial sub-level in order to reach a solution. Remember 
+that any Ruby object can be a hash key and any Ruby object can be a hash value.
+=end
