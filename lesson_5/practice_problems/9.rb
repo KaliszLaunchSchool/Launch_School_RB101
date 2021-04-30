fa16feabfb6adf648418227560e3d96924d0b61a
mskalisz <mskalisz3@gmail.com>
2021-04-30 15:11:45 -0400

    Add LS solution exercise 9

--- lesson_5/practice_problems/9.rb
+++ lesson_5/practice_problems/9.rb
@@ -30,3 +30,23 @@ arr.each do |subarray|
 end
 
 p arr
+
+=begin
+LS Solution
+
+arr.map do |sub_arr|
+  sub_arr.sort do |a, b|
+    b <=> a
+  end
+end
+
+# => [["c", "b", "a"], [3, 2, 1], ["green", "blue", "black"]]
+
+From the problem description we know that we need to return a new array but with the items in it transformed in some 
+way, so map should hopefully be a fairly obvious choice for the initial method to call on the array.
+
+We also know that we want each sub-array to be ordered, so within the block that we pass to map we can call sort on the 
+sub-array for that iteration. Since we want the sub-arrays sorted in reverse order we need to call it with a block, 
+within which we change the order of a and b.
+
+=end
