93e54aac6c12440b43fcc3973dacd1ea3ae5d961
mskalisz <mskalisz3@gmail.com>
2021-04-30 11:00:45 -0400

    Add LS solution exercise 8

--- lesson_5/practice_problems/8.rb
+++ lesson_5/practice_problems/8.rb
@@ -35,3 +35,37 @@ end
 
 words.flatten.each {|word| characters << word.chars}
 p characters.flatten.join.delete("^aeiou")
+
+=begin
+LS Solution
+
+vowels = 'aeiou'
+
+hsh.each do |_, value|
+  value.each do |str|
+    str.chars.each do |char|
+      puts char if vowels.include?(char)
+    end
+  end
+end
+# e
+# u
+# i
+# o
+# o
+# u
+# e
+# o
+# e
+# e
+# a
+# o
+The key here is to first iterate through the hash to access the values (the arrays) and then iterate through each array 
+to access each string. At this point String#chars can be called within the block to return an array of characters for 
+that string in that iteration, this array of characters can itself then be iterated through and puts called if on its 
+iteration the character meets a condition (in this case that it is present in the vowels variable).
+
+Note that we used each here, which means each iteration has an argument for a key and one for the value. Since we don't 
+care about the key, we just use _ to signify that this is an argument that we aren't going to use. We could also have 
+used Hash#values as well.
+=end
