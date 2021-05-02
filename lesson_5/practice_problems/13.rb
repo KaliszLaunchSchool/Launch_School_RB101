77919c904669c94e26dd79126b3c0a5f74501cd9
mskalisz <mskalisz3@gmail.com>
2021-05-02 10:04:29 -0400

    Add LS solution exercise 13

--- lesson_5/practice_problems/13.rb
+++ lesson_5/practice_problems/13.rb
@@ -34,3 +34,22 @@ arr.sort_by! do |subarray|
 end
 
 p arr
+
+=begin
+
+LS Solution
+
+arr.sort_by do |sub_arr|
+  sub_arr.select do |num|
+    num.odd?
+  end
+end
+# => [[1, 8, 3], [1, 6, 7], [1, 4, 9]]
+
+Since the sub-arrays are compared in an 'element-wise' manner when being sorted, when looking at the first element of 
+each they are all equal. If we were to include the second element of each (the even integers) in our comparison the 
+order would be different, since 4 is less than 6, which is less than 8.
+
+By performing selection on the sub-arrays that we are comparing, we can compare them based on the value of the odd 
+integers alone.
+=end
