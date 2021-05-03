00f0eeeb548ec5617f6cfb60966d4215be1a88b5
mskalisz <mskalisz3@gmail.com>
2021-05-02 20:22:07 -0400

    Add LS solution exercise 16

--- lesson_5/practice_problems/16.rb
+++ lesson_5/practice_problems/16.rb
@@ -63,3 +63,33 @@ end
 
 p UUID()
 
+=begin
+LS Solution
+
+def generate_UUID
+  characters = []
+  (0..9).each { |digit| characters << digit.to_s }
+  ('a'..'f').each { |digit| characters << digit }
+
+  uuid = ""
+  sections = [8, 4, 4, 4, 12]
+  sections.each_with_index do |section, index|
+    section.times { uuid += characters.sample }
+    uuid += '-' unless index >= sections.size - 1
+  end
+
+  uuid
+end
+
+The key to this solution is having the sections array containing integers representing the number of characters in each 
+section. During each iteration through the array, within the block we can call the times method on the integer for that 
+iteration and thus build up the UUID character by character.
+
+This is just one possible solution to the problem, and yours may be different. However you solved it though, you will 
+most likely have needed to use one or more iterative methods.
+
+Our solution ignores some complexity concerning the proper way to generate UUIDs. It has a higher probability of 
+creating conflicting values than you might think. See the RFC 4122 document for complete details about how to properly 
+generate UUIDs. Better yet, use an RFC 4122-compliant implementation of the algorithm, such as the SecureRandom#uuid 
+method in Ruby's standard library.
+=end
