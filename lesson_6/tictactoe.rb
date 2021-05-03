562b0d527aeb287d72d46eada6bad27cad351c21
mskalisz <mskalisz3@gmail.com>
2021-05-03 19:58:56 -0400

    Fix 'brd' variable, able to add X and O to correct places on board

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -11,25 +11,25 @@
 10. Good bye!
 =end
 
-def display_board(sq1='', sq2='')
+def display_board(brd)
   puts ""
   puts "     |     |"
-  puts "  #{brd[1]}  |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
   puts "     |     |"
   puts "-----+-----+-----"
   puts "     |     |"
-  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
   puts "     |     |"
   puts "-----+-----+-----"
   puts "     |     |"
-  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
   puts "     |     |"
   puts ""
 end
 # Board data structure will be a hash
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = "X"}
+  (1..9).each { |num| new_board[num] = ""}
   new_board
 end
 
