7fd15dd4b705bde7d63c30d82dbfa49bf8d5cbaa
mskalisz <mskalisz3@gmail.com>
2021-05-06 10:25:29 -0400

    Solve rubocop offences tic tac toe game

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -17,10 +17,15 @@ INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # coumns
+                [[1, 5, 9], [3, 5, 7]] # diagonals
+
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
   prompt "You're #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
@@ -38,31 +43,34 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop:enable Metrics/AbcSize
 
 # Board data structure will be a hash
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select{ |num| brd[num] == INITIAL_MARKER }
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece(brd)
   square = ''
   loop do
-    prompt "Choose a square (#{empty_squares(brd).join(", ")}):"
+    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
     square = gets.chomp.to_i
-    break if empty_squares(brd).include?(square) #if the user inputs a valid/avaiable choice
+    break if empty_squares(brd).include?(square)
+    # if the user inputs a valid/avaiable choice
     prompt "Sorry, that's not a valid choice"
   end
-  brd[square] = PLAYER_MARKER  # Save into board hash
+  brd[square] = PLAYER_MARKER # Save into board hash
 end
 
 def computer_places_piece(brd)
-  square = empty_squares(brd).sample #Represents an array of integers of what is available on the bord
+  square = empty_squares(brd).sample
+  # Represents an array of integers of what is available on the bord
   brd[square] = COMPUTER_MARKER
 end
 
@@ -71,22 +79,24 @@ def board_full?(brd)
 end
 
 def someone_won?(brd)
-  !!detect_winner(brd) #detect winner should return boolean, so use !! to be a boolean
+  !!detect_winner(brd)
+  # detect winner should return boolean, so use !! to be a boolean
 end
 
 def detect_winner(brd)
-  winning_lines = [[1,2,3], [4,5,6], [7,8,9]] + # rows
-                  [[1,4,7], [2,5,8], [3,6,9]] + # coumns
-                  [[1,5,9], [3,5,7]]            # diagonals
-  
-  winning_lines.each do |line| # line is an array
-    if brd[line[0]] == PLAYER_MARKER && 
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
-       return 'Player'
-    elsif brd[line[0]] == PLAYER_MARKER && 
-      brd[line[1]] == PLAYER_MARKER &&
-      brd[line[2]] == PLAYER_MARKER
+  WINNING_LINES.each do |line| # line is an array
+    # if brd[line[0]] == PLAYER_MARKER &&
+    #    brd[line[1]] == PLAYER_MARKER &&
+    #    brd[line[2]] == PLAYER_MARKER
+    #   return 'Player'
+    # elsif brd[line[0]] == PLAYER_MARKER &&
+    #       brd[line[1]] == PLAYER_MARKER &&
+    #       brd[line[2]] == PLAYER_MARKER
+    #   return 'Computer'
+    # end
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(line[0], line[1], line[2]).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
   end
@@ -94,7 +104,8 @@ def detect_winner(brd)
 end
 
 loop do
-  board = initialize_board #will keep track of state of the game
+  board = initialize_board
+  # will keep track of state of the game
 
   loop do
     display_board(board)
@@ -109,7 +120,8 @@ loop do
   display_board(board)
 
   if someone_won?(board)
-    prompt "#{detect_winner(board)} won!" # detect winner should return a string
+    prompt "#{detect_winner(board)} won!"
+    # detect winner should return a string
   else
     prompt "It's a tie!"
   end
