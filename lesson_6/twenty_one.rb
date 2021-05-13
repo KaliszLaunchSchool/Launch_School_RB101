4c1564040c182b8d33b426a810e18534a8de3795
mskalisz <mskalisz3@gmail.com>
2021-05-13 13:24:29 -0400

    Calculate value of each hand, need to create method for calculating value of ace

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -10,8 +10,12 @@
 7. Compare cards and declare winner.
 =end
 
+require 'pry'
+
 values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
 suits = ['hearts', 'diamonds', 'clubs', 'spades']
+player_cards = []
+dealer_cards = []
 
 def initiate_deck(values, suits)
   deck = {}
@@ -22,28 +26,45 @@ def initiate_deck(values, suits)
   deck
 end
 
-def deal_cards(deck, suits)
-  player_cards = []
-  dealer_cards = []
+def deal_cards(deck, suits, player_cards, dealer_cards)
   loop do
-    player_cards << one_card(deck, suits)
-    dealer_cards << one_card(deck, suits)
+    player_cards << deal_one_card(deck, suits)
+    dealer_cards << deal_one_card(deck, suits)
     break if dealer_cards.size == 2
   end
-  player_cards
-  dealer_cards
-
+  p player_cards
+  p dealer_cards
 end
 
-def one_card(deck, suits)
+def deal_one_card(deck, suits)
   card = {}
   chosen_suit = suits.sample
   chosen_value = deck.fetch(chosen_suit).sample
   card[chosen_suit] = chosen_value
-  p card
-  p deck[chosen_suit].delete_if {|value| value == chosen_value}
-  p deck
+  
+  deck[chosen_suit].delete_if {|value| value == chosen_value}
+  card
+end
+
+def calculate_hand(cards)
+  values = []
+  cards.each do |card|
+    card_value = card.values[0]
+    if card_value == 'ace'
+      values << 1
+    elsif card_value.class == String
+      values << 10
+    else
+      values << card_value
+    end
+  end
+  p values.sum
+end
+
+def calculate_ace(cards)
 end
 
 deck = initiate_deck(values, suits)
-deal_cards(deck, suits)
+deal_cards(deck, suits, player_cards, dealer_cards)
+p calculate_hand(player_cards)
+p calculate_hand(dealer_cards)
