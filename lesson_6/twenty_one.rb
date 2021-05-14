1979ddb9f5d1e87b050b317ba19c82f63cc060ae
mskalisz <mskalisz3@gmail.com>
2021-05-14 15:39:37 -0400

    Rubocop cleanup

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -13,14 +13,14 @@
 require 'pry'
 
 deck = {}
-values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'ace', 'ace', 'ace']
+values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
 suits = ['hearts', 'diamonds', 'clubs', 'spades']
 player_cards = []
 dealer_cards = []
 
 def welcome
   prompt("Welcome to the Twenty-One game!")
-  prompt("Try to get as close to 21 as possible, without going over.") 
+  prompt("Try to get as close to 21 as possible, without going over.")
   prompt("If you go over 21, it's a 'bust' and you lose.")
   enter_to_continue
   prompt("Face cards are 10 points, and aces are either 1 point or 11.")
@@ -29,7 +29,7 @@ def welcome
 end
 
 def initiate_deck(deck, values, suits)
-  for suit in suits
+  suits.each do |suit|
     deck[suit] = values.dup
   end
   deck
@@ -41,8 +41,6 @@ def deal_cards(deck, suits, player_cards, dealer_cards)
     dealer_cards << deal_one_card(deck, suits)
     break if dealer_cards.size == 2
   end
-  player_cards
-  dealer_cards
   deck
 end
 
@@ -59,25 +57,25 @@ def calculate_hand(cards)
   values = []
   cards.each do |card|
     card_value = card.values[0]
-    unless card_value == 'ace' 
+    unless card_value == 'ace'
       if card_value.class == String
-      values << 10
+        values << 10
       else
         values << card_value
       end
     end
     if card_value == 'ace'
-      values << calculate_ace(cards, values, card_value)
+      values << calculate_ace(values)
     end
   end
   values.sum
 end
 
-def calculate_ace(cards, values, card_value)
+def calculate_ace(values)
   if (values.sum + 11) > 21
-    card_value = 1
+    1
   else
-    card_value = 11
+    11
   end
 end
 
@@ -97,8 +95,8 @@ def player_turn(player_cards, dealer_cards, deck, suits)
     player_cards << deal_one_card(deck, suits)
     prompt("You have:")
     display_cards(player_cards)
-    if bust?(player_cards) 
-      prompt('Bust!') 
+    if bust?(player_cards)
+      prompt('Bust!')
       break
     end
   end
@@ -116,7 +114,7 @@ def hit_or_stay
   end
   if hit_or_stay_response == 'h'
     hit_or_stay_response = 'hit'
-  elsif hit_or_stay_response ==  's'
+  elsif hit_or_stay_response == 's'
     hit_or_stay_response = 'stay'
   else
     hit_or_stay_response
@@ -140,7 +138,6 @@ def bust?(cards)
 end
 
 def dealer_turn(dealer_cards, deck, suits)
-  dealer_cards
   dealer_score = calculate_hand(dealer_cards)
 
   loop do
@@ -153,9 +150,7 @@ def dealer_turn(dealer_cards, deck, suits)
     else 
       prompt("The dealer hits!")
       dealer_cards << deal_one_card(deck, suits)
-      dealer_cards
       dealer_score = calculate_hand(dealer_cards)
-      dealer_score
     end
     enter_to_continue
   end
@@ -164,7 +159,7 @@ end
 def determine_winner(player_cards, dealer_cards, deck, suits)
   if bust?(player_cards)
     enter_to_continue
-    prompt("You bust! The dealer wins.") 
+    prompt("You bust! The dealer wins.")
   else
     dealer_turn(dealer_cards, deck, suits)
     if bust?(dealer_cards)
@@ -186,7 +181,7 @@ def show_dealer_hand(dealer_cards)
   prompt("The dealer had #{calculate_hand(dealer_cards)} points.")
 end
 
-def display_final_cards (player_cards, dealer_cards)
+def display_final_cards(player_cards, dealer_cards)
   prompt("You had:")
   display_cards(player_cards)
   prompt("The dealer had:")
@@ -219,7 +214,7 @@ end
 
 system 'clear'
 welcome
-  loop do
+loop do
   system 'clear'
   initiate_deck(deck, values, suits)
   deal_cards(deck, suits, player_cards, dealer_cards)
