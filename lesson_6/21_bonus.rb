99d1c527d8a979bf3d3cc7617473bf075e9d578b
mskalisz <mskalisz3@gmail.com>
2021-05-16 17:40:04 -0400

    Keep track of round winner and play until round winner reaches a certain number

--- lesson_6/21_bonus.rb
+++ lesson_6/21_bonus.rb
@@ -1,5 +1,7 @@
+require 'pry'
 SUITS = ['H', 'D', 'S', 'C']
 VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+WINNING_ROUNDS = 2
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -73,9 +75,16 @@ end
 
 def play_again?
   puts "-------------"
-  prompt "Do you want to play again? (y or n)"
-  answer = gets.chomp
-  answer.downcase.start_with?('y')
+  prompt("Would you like to play again?")
+  play_again = ''
+
+  loop do
+    play_again = gets.chomp.downcase
+    break if ['y', 'yes', 'n', 'no'].include?(play_again)
+    prompt('Please enter valid answer (y/yes or n/no)')
+  end
+
+  ['y', 'yes'].include?(play_again)
 end
 
 def display_match_results(dealer_cards, player_cards, dealer_total, player_total)
@@ -85,78 +94,138 @@ def display_match_results(dealer_cards, player_cards, dealer_total, player_total
   puts "=============="
 end
 
+def update_scoreboard(dealer_cards, player_cards, scoreboard)
+  result = detect_result(dealer_cards, player_cards)
+
+  result =  case result
+            when :player_busted
+              :Dealer
+            when :dealer_busted
+              :Player
+            when :player
+              :Player
+            when :dealer
+              :Dealer
+            when :tie
+              nil
+            end
+
+  scoreboard[result] += 1 unless result == nil
+end
+
+def display_round_score(scoreboard)
+  prompt("Current score")
+  prompt("  Player: #{scoreboard['Player'.to_sym]}")
+  prompt("  Dealer: #{scoreboard['Dealer'.to_sym]}")
+end
+
+def match_over?(score)
+  score.value?(WINNING_ROUNDS)
+end
+
+def enter_to_continue
+  prompt('Press enter to continue')
+  STDIN.gets
+end
+
+def enter_to_continue_next_round
+  prompt('Press enter to continue to the next round')
+  STDIN.gets
+end
+
+def all_results(dealer_cards, player_cards, scoreboard)
+  enter_to_continue
+  system 'clear'
+  update_scoreboard(dealer_cards, player_cards, scoreboard)
+  display_round_score(scoreboard)
+  if match_over?(scoreboard)
+    enter_to_continue
+    system 'clear'
+    true
+  else
+    enter_to_continue_next_round
+    system 'clear'
+    false
+  end
+end
+
 loop do
   prompt "Welcome to Twenty-One!"
+  prompt "The first player to win #{WINNING_ROUNDS} games, wins!"
 
-  # initialize vars
-  deck = initialize_deck
-  player_cards = []
-  dealer_cards = []
-
-  # initial deal
-  2.times do
-    player_cards << deck.pop
-    dealer_cards << deck.pop
-  end
-
-  player_total = total(player_cards)
-  dealer_total = total(dealer_cards)
-
-  prompt "Dealer has #{dealer_cards[0]} and ?"
-  prompt "You have: #{player_cards[0]} and #{player_cards[1]}, for a total of #{player_total}."
-
-  # player turn
+  scoreboard = { Player: 0, Dealer: 0 }
   loop do
-    player_turn = nil
-    loop do
-      prompt "Would you like to (h)it or (s)tay?"
-      player_turn = gets.chomp.downcase
-      break if ['h', 's'].include?(player_turn)
-      prompt "Sorry, must enter 'h' or 's'."
-    end
 
-    if player_turn == 'h'
+    # initialize vars
+    deck = initialize_deck
+    player_cards = []
+    dealer_cards = []
+
+    # initial deal
+    2.times do
       player_cards << deck.pop
-      prompt "You chose to hit!"
-      prompt "Your cards are now: #{player_cards}"
-      prompt "Your total is now: #{player_total = total(player_cards)}"
+      dealer_cards << deck.pop
     end
 
-    break if player_turn == 's' || busted?(player_cards)
-  end
+    player_total = total(player_cards)
+    dealer_total = total(dealer_cards)
 
-  if busted?(player_cards)
+    prompt "Dealer has #{dealer_cards[0]} and ?"
+    prompt "You have: #{player_cards[0]} and #{player_cards[1]}, for a total of #{player_total}."
+
+    # player turn
+    loop do
+      player_turn = nil
+      loop do
+        prompt "Would you like to (h)it or (s)tay?"
+        player_turn = gets.chomp.downcase
+        break if ['h', 's'].include?(player_turn)
+        prompt "Sorry, must enter 'h' or 's'."
+      end
+
+      if player_turn == 'h'
+        player_cards << deck.pop
+        prompt "You chose to hit!"
+        prompt "Your cards are now: #{player_cards}"
+        prompt "Your total is now: #{player_total = total(player_cards)}"
+      end
+
+      break if player_turn == 's' || busted?(player_cards)
+    end
+
+    if busted?(player_cards)
+      display_match_results(dealer_cards, player_cards, dealer_total, player_total)
+      display_result(dealer_cards, player_cards)
+      all_results(dealer_cards, player_cards, scoreboard) ? break : next
+    else
+      prompt "You stayed at #{player_total}"
+    end
+
+    # dealer turn
+    prompt "Dealer turn..."
+
+    loop do
+      break if dealer_total >= 17
+
+      prompt "Dealer hits!"
+      dealer_cards << deck.pop
+      prompt "Dealer's cards are now: #{dealer_total = total(dealer_cards)}"
+    end
+
+    if busted?(dealer_cards)
+      prompt "Dealer total is now: #{dealer_cards}"
+      display_match_results(dealer_cards, player_cards, dealer_total, player_total)
+      display_result(dealer_cards, player_cards)
+      all_results(dealer_cards, player_cards, scoreboard) ? break : next
+    else
+      prompt "Dealer stays at #{dealer_total}"
+    end
+
+    # both player and dealer stays - compare cards!
     display_match_results(dealer_cards, player_cards, dealer_total, player_total)
-    display_result(dealer_cards, player_cards)
-    play_again? ? next : break
-  else
-    prompt "You stayed at #{player_total}"
+    
+    break if all_results(dealer_cards, player_cards, scoreboard)
   end
-
-  # dealer turn
-  prompt "Dealer turn..."
-
-  loop do
-    break if dealer_total >= 17
-
-    prompt "Dealer hits!"
-    dealer_cards << deck.pop
-    prompt "Dealer's cards are now: #{dealer_total = total(dealer_cards)}"
-  end
-
-  if busted?(dealer_cards)
-    prompt "Dealer total is now: #{dealer_cards}"
-    display_match_results(dealer_cards, player_cards, dealer_total, player_total)
-    display_result(dealer_cards, player_cards)
-    play_again? ? next : break
-  else
-    prompt "Dealer stays at #{dealer_total}"
-  end
-
-  # both player and dealer stays - compare cards!
-  display_match_results(dealer_cards, player_cards, dealer_total, player_total)
-
-  display_result(dealer_cards, player_cards)
 
   break unless play_again?
 end
