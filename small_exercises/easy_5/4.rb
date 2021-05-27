c4634f5ccad33d6cd817878b3b7f3cb4db288e5c
mskalisz <mskalisz3@gmail.com>
2021-05-27 09:16:25 -0400

    Attempt to explain code in exercise 4, update explaining_code.md file

--- small_exercises/easy_5/4.rb
+++ small_exercises/easy_5/4.rb
@@ -53,3 +53,19 @@ end
 p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
 p swap('Abcde') == 'ebcdA'
 p swap('a') == 'a'
+
+=begin
+On `lines 40–51` we are defining the method `swap` which takes 1 parameter.
+
+In `line 41` we initiaite variable `words` and assign it to the return value of calling the method 'split'
+  on `sentence`. This returns a new array, which is stored in our variable
+
+On `lines 42-49` we invoke the map method on the local variable words and pass in the do..end block as an 
+  argument. As the map method iterates over each element in the array, it swaps the letters. 
+  Somethign about variables as pointers
+
+On `line 53`, `line 54`, and `line 55` we are calling the method `swap` and passing in the String object 
+`sentence` as an argument to it. We compare the return value of `swap` to our anticipated return value. 
+They are compared with the `==` operator, which will return `true` or `false`. In Ruby, everything is 
+considered `truthy` unless `false` or `nil`, and so `lines 53-55` evaluate as 'true', and so 'true is output'
+=end
