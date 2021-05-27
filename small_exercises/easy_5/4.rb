382920b0500fa4a21970e6a84199d4f30ff17dfd
mskalisz <mskalisz3@gmail.com>
2021-05-27 09:31:57 -0400

    Add LS solution exercise 4

--- small_exercises/easy_5/4.rb
+++ small_exercises/easy_5/4.rb
@@ -40,11 +40,10 @@ swap('a') == 'a'
 def swap(sentence)
   words = sentence.split(/ /)
   words.map do |word|
-    last_index = word.size - 1
     first_letter  = word[0]
-    last_letter = word[last_index]
+    last_letter = word[-1]
     word[0] = last_letter
-    word[last_index] = first_letter
+    word[-1] = first_letter
     word
   end
   words.join(' ')
@@ -68,4 +67,57 @@ On `line 53`, `line 54`, and `line 55` we are calling the method `swap` and pass
 `sentence` as an argument to it. We compare the return value of `swap` to our anticipated return value. 
 They are compared with the `==` operator, which will return `true` or `false`. In Ruby, everything is 
 considered `truthy` unless `false` or `nil`, and so `lines 53-55` evaluate as 'true', and so 'true is output'
+=end
+
+=begin
+LS Solution
+
+Using substrings
+
+def swap_first_last_characters(word)
+  word[0], word[-1] = word[-1], word[0]
+  word
+end
+
+def swap(words)
+  result = words.split.map do |word|
+    swap_first_last_characters(word)
+  end
+  result.join(' ')
+end
+Discussion
+Our solution takes a straightforward approach; it simply chops the string into words, then iterates through 
+all of the words using map, and produces an array of modified words. We then apply join to that array to 
+produce our return value.
+
+The tricky part here is actually swapping the first and last characters of each word. Because of the mild 
+trickiness, we use a separate method. It uses this common idiom:
+
+a, b = b, a
+to exchange the values of a and b. In our method, a is the first letter of the word, and b is the last letter 
+of the word. When Ruby sees something like this, it effectively creates a temporary array with the values from 
+the right side ([b, a]), and then assigns each element from that array into the corresponding named variable:
+
+a = b   # b is value from position 0 of temporary array (original value of b)
+b = a   # a is value from position 1 of temporary array (original value of a)
+Looked at another way, this is equivalent to:
+
+temporary = [b, a]
+a = temporary[0]
+b = temporary[1]
+This multiple-assignment syntax is powerful and complex, and it sometimes surprises people who haven't seen it 
+before. Nevertheless, you should at least learn and use this idiomatic form of the syntax, but beware of using 
+the more complex forms allowed by Ruby.
+
+Further Exploration
+How come our solution passes word into the swap_first_last_characters method invocation instead of just 
+passing the characters that needed to be swapped? Suppose we had this implementation:
+
+def swap_first_last_characters(a, b)
+  a, b = b, a
+end
+and called the method like this:
+
+swap_first_last_characters(word[0], word[-1])
+Would this method work? Why or why not?
 =end
