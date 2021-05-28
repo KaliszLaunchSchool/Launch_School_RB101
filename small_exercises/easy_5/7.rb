f0fdcb502fc57c26f76dbcede04cb726dcbacfe5
mskalisz <mskalisz3@gmail.com>
2021-05-28 12:41:06 -0400

    Add LS solution exercise 7

--- small_exercises/easy_5/7.rb
+++ small_exercises/easy_5/7.rb
@@ -58,3 +58,25 @@ p word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 2 }
 p word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 3 }
 p word_sizes("What's up doc?") == { 5 => 1, 2 => 1, 3 => 1 }
 p word_sizes('') == {}
+
+=begin
+LS solution
+
+def word_sizes(words_string)
+  counts = Hash.new(0)
+  words_string.split.each do |word|
+    clean_word = word.delete('^A-Za-z')
+    counts[clean_word.size] += 1
+  end
+  counts
+end
+
+Discussion
+The only change we need to make to this method is to make sure that it does not count non-letter characters 
+in determining word sizes. This is easy to do: we simply delete all non-letters from each word before we 
+compute the size.
+
+Further Exploration
+If you haven't encountered String#delete before, take a few minutes to read up on it, and its companion 
+String#count (you need to read about #count to get all of the information you need to understand #delete).
+=end
