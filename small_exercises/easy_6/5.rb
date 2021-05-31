05b3186cba9ff7d2ed90853d02ee1361019c9e31
mskalisz <mskalisz3@gmail.com>
2021-05-31 19:48:46 -0400

    Add LS solution exercise 5

--- small_exercises/easy_6/5.rb
+++ small_exercises/easy_6/5.rb
@@ -71,3 +71,26 @@ p new_list = reverse(list)              # => [2, 3, 1]
 p list.object_id != new_list.object_id  # => true
 p list == [1, 3, 2]                     # => true
 p new_list == [2, 3, 1]                 # => true
+
+=begin
+LS Solution
+
+def reverse(array)
+  result_array = []
+  array.reverse_each { |element| result_array << element }
+  result_array
+end
+
+Discussion
+Well, nobody said reverse_each wasn't allowed! We could have solved this in a manner similar to the previous 
+exercise, but where would the fun be in that? reverse_each is like each, except it processes the elements in 
+reverse order. Note that unlike each, reverse_each only applies to Arrays.
+
+Note in particular that our tests include list.object_id != new_list.object_id. This verifies that the returned 
+array is a brand new array, and not a modified form of the original way. Likewise, the second test of 
+list == [1, 3, 2] confirms that the original array hasn't changed.
+
+Further Exploration
+An even shorter solution is possible by using either inject or each_with_object. Just for fun, read about these 
+methods in the Enumerable module documentation, and try using one in your reverse method.
+=end
