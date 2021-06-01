fdb6c849d9d039f0f765033ca472e68da192ec97
mskalisz <mskalisz3@gmail.com>
2021-05-31 20:04:13 -0400

    Add LS solution exercise 6

--- small_exercises/easy_6/6.rb
+++ small_exercises/easy_6/6.rb
@@ -34,3 +34,19 @@ def merge(array_1, array_2)
 end
 
 p merge([1, 3, 5], [3, 6, 9]) == [1, 3, 5, 6, 9]
+
+=begin
+LS Solution
+
+def merge(array_1, array_2)
+  array_1 | array_2
+end
+Discussion
+In this exercise we utilize the Ruby method Array#|. This is a method that gives the set union of two arrays, 
+so the result contains no duplicates. It works perfectly for our merge method. The union part of this method 
+means that we get back all the elements that both array_1 and array_2 might contain, but without duplicates.
+
+Further Exploration
+How did you go about solving this one? Using this method for set union is convenient, but what if we didn't h
+ave it? There are certainly a few other ways to go about solving this exercise.
+=end
