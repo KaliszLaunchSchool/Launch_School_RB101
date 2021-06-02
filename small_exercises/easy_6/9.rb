11c850953b0b59d26127f331c034f101b32a0af4
mskalisz <mskalisz3@gmail.com>
2021-06-02 07:28:32 -0400

    Add LS solution exercise 9

--- small_exercises/easy_6/9.rb
+++ small_exercises/easy_6/9.rb
@@ -46,3 +46,32 @@ p include?([1,2,3,4,5], 6) == false
 p include?([], 3) == false
 p include?([nil], nil) == true
 p include?([], nil) == false
+
+=begin
+LS Solution
+
+The easy way
+
+def include?(array, value)
+  !!array.find_index(value)
+end
+
+A slightly harder way
+
+def include?(array, value)
+  array.each { |element| return true if value == element }
+  false
+end
+
+Discussion
+The first method uses Array#find_index to scan the array for the first element that has the specified value. 
+find_index returns the index number of the found element, which will always have a truthy value, or nil if no 
+such element is present. We then use !! to force the return value to true or false in accordance with the 
+implied promise of the ? in include?.
+
+The second method does essentially the same thing, but it uses each instead of find_index. This takes a little 
+bit more work, since we are explicitly returning false if the value is not found.
+
+Further Exploration
+Can you think of other solutions to this problem? There are lots of different ways to get the same result.
+=end
