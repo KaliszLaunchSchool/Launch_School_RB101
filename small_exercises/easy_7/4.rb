05f7a1b9fa3bbc54591aedd7d276a31bf06dfa5a
mskalisz <mskalisz3@gmail.com>
2021-06-04 15:00:08 -0400

    Add LS solution exercise 4

--- small_exercises/easy_7/4.rb
+++ small_exercises/easy_7/4.rb
@@ -40,7 +40,7 @@ UPPERCASE_ALPHABET = ("A".."Z").to_a
 LOWERCASE_ALPHABET = ("a".."z").to_a
 
 def swapcase(phrase)
-  swapped = phrase.chars.map do |character|
+  swapped_chars = phrase.chars.map do |character|
     if UPPERCASE_ALPHABET.include?(character)
       character.downcase
     elsif LOWERCASE_ALPHABET.include?(character)
@@ -49,8 +49,33 @@ def swapcase(phrase)
       character
     end
   end
-  swapped.join
+  swapped_chars.join
 end
 
 p swapcase('CamelCase') == 'cAMELcASE'
 p swapcase('Tonight on XYZ-TV') == 'tONIGHT ON xyz-tv'
+
+=begin
+LS Solution
+
+def swapcase(string)
+  characters = string.chars.map do |char|
+    if char =~ /[a-z]/
+      char.upcase
+    elsif char =~ /[A-Z]/
+      char.downcase
+    else
+      char
+    end
+  end
+  characters.join
+end
+
+Discussion
+In our solution, we iterate through each character in the string passed into swapcase. If the character is 
+lowercase (/[a-z]/), we make it uppercase; if it is uppercase (/[A-Z]/), we make it lowercase. For all other 
+characters, we do nothing.
+
+Each character is then mapped to a new array and assigned to characters. Finally, we join characters together 
+into a new String and return that value.
+=end
