c4eef09a269c94fa9a29b6004343484e0cc99c12
mskalisz <mskalisz3@gmail.com>
2021-06-04 10:43:48 -0400

    Add LS solution execise 10

--- small_exercises/easy_6/10.rb
+++ small_exercises/easy_6/10.rb
@@ -87,3 +87,31 @@ end
 
 triangle(5)
 triangle(9)
+
+=begin
+LS Solution
+
+def triangle(num)
+  spaces = num - 1
+  stars = 1
+
+  num.times do |n|
+    puts (' ' * spaces) + ('*' * stars)
+    spaces -= 1
+    stars += 1
+  end
+end
+
+Discussion
+For this problem we have an inverse relationship between the number of spaces and the number of stars that 
+should be printed on each line. If we are on the first line, we print num - 1 spaces and 1 star. If we are on 
+the second line we print num - 2 spaces and 2 stars. And so forth.
+
+We do just that by initially setting stars to 1, and spaces to num -1. On each iteration, we output the number 
+of spaces and stars, and then we decrement the number of spaces and increment the number of stars. This will 
+give us the desired result.
+
+Further Exploration
+Try modifying your solution so it prints the triangle upside down from its current orientation. Try modifying 
+your solution again so that you can display the triangle with the right angle at any corner of the grid.
+=end
