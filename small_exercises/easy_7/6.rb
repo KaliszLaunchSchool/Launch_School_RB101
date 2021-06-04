3891ca0b65e503ff905aa8a2b1ae3fc36d8898cb
mskalisz <mskalisz3@gmail.com>
2021-06-04 16:14:30 -0400

    Add LS solution exercise 6

--- small_exercises/easy_7/6.rb
+++ small_exercises/easy_7/6.rb
@@ -55,3 +55,37 @@ end
 p staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
 p staggered_case('ALL CAPS') == 'AlL cApS'
 p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
+
+=begin
+LS Solution
+
+def staggered_case(string)
+  result = ''
+  need_upper = true
+  string.chars.each do |char|
+    if char =~ /[a-z]/i
+      if need_upper
+        result += char.upcase
+      else
+        result += char.downcase
+      end
+      need_upper = !need_upper
+    else
+      result += char
+    end
+  end
+  result
+end
+
+Discussion
+This solution is very similar to the previous solution; the only difference is that we need to avoid changing 
+need_upper when processing non-alphabetic characters. We use a simple regular expression with the /i flag 
+(ignore case) to detect letters.
+
+Further Exploration
+Modify this method so the caller can determine whether non-alphabetic characters should be counted when 
+determining the upper/lowercase state. That is, you want a method that can perform the same actions that this 
+method does, or operates like the previous version.
+
+Hint: Use a keyword argument.
+=end
