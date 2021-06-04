426f8256a43b22d9536e997b467ceab1ea3bb7fa
mskalisz <mskalisz3@gmail.com>
2021-06-04 12:30:28 -0400

    Add LS solution exercuse 3

--- small_exercises/easy_7/3.rb
+++ small_exercises/easy_7/3.rb
@@ -48,3 +48,36 @@ end
 p word_cap('four score and seven') == 'Four Score And Seven'
 p word_cap('the javaScript language') == 'The Javascript Language'
 p word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
+
+=begin
+
+LS Solution
+
+def word_cap(words)
+  words_array = words.split.map do |word|
+    word.capitalize
+  end
+  words_array.join(' ')
+end
+
+We can also write this more succinctly as:
+
+def word_cap(words)
+  words.split.map(&:capitalize).join(' ')
+end
+
+Discussion
+In the second solution, (&:method_name) is shorthand notation for { |item| item.method_name }. Thus, 
+&:capitalize translates to:
+
+Copy Code
+do |word|
+  word.capitalize
+end
+
+We can use this shorthand syntax with any method that has no required arguments.
+
+Further Exploration
+Ruby conveniently provides the String#capitalize method to capitalize strings. Without that method, how would 
+you solve this problem? Try to come up with at least two solutions.
+=end
