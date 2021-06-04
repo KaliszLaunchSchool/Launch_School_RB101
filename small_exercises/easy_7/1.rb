9ae6311657aedf42b7e7bf8873fb346399e720c4
mskalisz <mskalisz3@gmail.com>
2021-06-04 11:15:23 -0400

    Add LS solution exercise 1

--- small_exercises/easy_7/1.rb
+++ small_exercises/easy_7/1.rb
@@ -43,3 +43,24 @@ def interleave(first_array, second_array)
 end
 
 p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
+
+=begin
+LS Solution
+
+def interleave(array1, array2)
+  result = []
+  array1.each_with_index do |element, index|
+    result << element << array2[index]
+  end
+  result
+end
+
+Discussion
+In our solution, we use #each_with_index to get values and index numbers for array1, then use the index number 
+to access the corresponding element in array2.
+
+Further Exploration
+Take a few minutes to read about Array#zip. #zip doesn't do the same thing as interleave, but it is very close, 
+and more flexible. In fact, interleave can be implemented in terms of zip and one other method from the Array 
+class. See if you can rewrite interleave to use zip.
+=end
