ed4ff2210a509687653964e3e6ba90ef9d503665
mskalisz <mskalisz3@gmail.com>
2021-06-06 18:38:18 -0400

    Add LS solution exercise 10

--- small_exercises/easy_7/10.rb
+++ small_exercises/easy_7/10.rb
@@ -43,3 +43,32 @@ end
 
 p penultimate('last word') == 'last'
 p penultimate('Launch School is great!') == 'is'
+
+=begin
+LS Solution
+
+def penultimate(words)
+  words_array = words.split
+  words_array[-2]
+end
+
+Discussion
+In this problem we first break our words up and put them into an array. Anything that isn't whitespace is 
+considered a word, so we can just use split to divvy up the words. Then, we use a negative index to grab the 
+second to last object in our words array and return it to the caller.
+
+Just in case you aren't too familiar with negative indices, they start from the last item in an array and 
+continue back through the array from there.
+
+-1 : last item in array. -2 : second to last item in the array.
+
+and so on.
+
+Further Exploration
+Our solution ignored a couple of edge cases because we explicitly stated that you didn't have to handle them: 
+strings that contain just one word, and strings that contain no words.
+
+Suppose we need a method that retrieves the middle word of a phrase/sentence. What edge cases need to be 
+considered? How would you handle those edge cases without ignoring them? Write a method that returns the middle 
+word of a phrase or sentence. It should handle all of the edge cases you thought of.
+=end
