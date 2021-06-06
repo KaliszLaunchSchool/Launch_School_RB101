2d2567fd06456405205d3c014a58c3977aa0b5a3
mskalisz <mskalisz3@gmail.com>
2021-06-06 18:34:23 -0400

    Solve exercise 10

--- small_exercises/easy_7/10.rb
+++ small_exercises/easy_7/10.rb
@@ -1,1 +1,45 @@
+=begin
+The End Is Near But Not Here
+Write a method that returns the next to last word in the String passed to it as an argument.
 
+Words are any sequence of non-blank characters.
+
+You may assume that the input String will always contain at least two words.
+
+Examples:
+
+penultimate('last word') == 'last'
+penultimate('Launch School is great!') == 'is'
+
+# Problem
+- Write a method which accepts 1 parameter (string)
+- Returns the second to last word in the string
+- Word = sequence of non-blank characters
+- Assume string will always contain at least 2 words
+
+Input: String
+Output: string
+
+# Exercises
+penultimate('last word') == 'last'
+penultimate('Launch School is great!') == 'is'
+
+# Data
+- String
+- Array
+
+# Algo
+- Create a method which accepts 1 paarameter (string)
+- Split the string by the spaces into an array 
+- Return Array[-2]
+
+# Code
+=end
+
+def penultimate(string)
+  string.split(' ')[-2]
+
+end
+
+p penultimate('last word') == 'last'
+p penultimate('Launch School is great!') == 'is'
