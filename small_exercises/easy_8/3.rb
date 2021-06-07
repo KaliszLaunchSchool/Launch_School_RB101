93a76456aca19c8bf9bc8dd0e683e90acbc43de5
mskalisz <mskalisz3@gmail.com>
2021-06-06 21:52:23 -0400

    Add LS solution exercise 3

--- small_exercises/easy_8/3.rb
+++ small_exercises/easy_8/3.rb
@@ -51,3 +51,26 @@ end
 p leading_substrings('abc') == ['a', 'ab', 'abc']
 p leading_substrings('a') == ['a']
 p leading_substrings('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
+
+=begin
+LS Solution
+
+def leading_substrings(string)
+  result = []
+  0.upto(string.size - 1) do |index|
+    result << string[0..index]
+  end
+  result
+end
+
+Discussion
+This problem is straightforward; all we need to do is to iterate through the characters of a string, returning 
+each substring that starts at the beginning of the string, and ends with the character we are currently 
+iterating.
+
+The hardest part of this problem is determining the correct looping structure to use; we decided to use upto, 
+but any looping structure that works would be fine.
+
+The expression string[0..index] may need a bit of explanation; it simply returns a substring of string. The 
+substring starts at position 0, and ends at position index.
+=end
