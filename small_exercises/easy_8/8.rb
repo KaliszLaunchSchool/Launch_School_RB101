1ee30f69d493daa3a6e9786445d9e73582ecb7cb
mskalisz <mskalisz3@gmail.com>
2021-06-10 18:24:09 -0400

    Add LS solution exercise 8

--- small_exercises/easy_8/8.rb
+++ small_exercises/easy_8/8.rb
@@ -64,3 +64,24 @@ p double_consonants('String') == "SSttrrinngg"
 p double_consonants("Hello-World!") == "HHellllo-WWorrlldd!"
 p double_consonants("July 4th") == "JJullyy 4tthh"
 p double_consonants('') == ""
+
+=begin
+LS Solution
+
+CONSONANTS = %w(b c d f g h j k l m n p q r s t v w x y z)
+
+def double_consonants(string)
+  result = ''
+  string.each_char do |char|
+    result << char
+    result << char if CONSONANTS.include?(char.downcase)
+  end
+  result
+end
+Discussion
+This exercise is nearly identical to the previous exercise, so it should come as no surprise that the solution 
+is also similar. The main difference here is that we need to check each character to see if it is a consonant. 
+There are lots of ways to do this: we have chosen to use an array of the lowercase consonants, and just check 
+each character to see if it is in that array. Of course, we do need to account for uppercase consonants as well, 
+so we convert each character to lowercase for the test.
+=end
