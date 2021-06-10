cdcc14e5af2342f068ab9dadae41aa0293df3c1f
mskalisz <mskalisz3@gmail.com>
2021-06-10 10:43:15 -0400

    Add LS solution exercise 7

--- small_exercises/easy_8/7.rb
+++ small_exercises/easy_8/7.rb
@@ -46,3 +46,20 @@ end
 p repeater('Hello') == "HHeelllloo"
 p repeater("Good job!") == "GGoooodd  jjoobb!!"
 p repeater('') == ''
+
+=begin
+LS Solution
+
+def repeater(string)
+  result = ''
+  string.each_char do |char|
+    result << char << char
+  end
+  result
+end
+
+Discussion
+This solution is straightforward. We initialize a result string, then iterate through the original string 
+while appending each character to the result 2 times. Finally, we return the result string.
+
+=end
