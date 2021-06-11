8f64458ea9c89cd1df08bb01bcf3a1c009e59db1
mskalisz <mskalisz3@gmail.com>
2021-06-11 16:30:58 -0400

    Add LS solution exercise 3

--- small_exercises/easy_9/3.rb
+++ small_exercises/easy_9/3.rb
@@ -47,3 +47,33 @@ end
 p negative(5) == -5
 p negative(-3) == -3
 p negative(0) == 0 
+
+=begin
+LS Solution
+
+def negative(number)
+  number > 0 ? -number : number
+end
+
+Discussion
+There are only two choices here. Either make the number negative and return it or return the already negative 
+number. This is the perfect place for a ternary.
+
+The condition will check if the number is greater than zero. If so make it negative and return it; otherwise, 
+return the already negative number.
+
+Further Exploration
+There is an even shorter way to write this but it isn't as immediately intuitive.
+
+def negative(number)
+  -number.abs
+end
+This works by flipping the problem on the head. It straightaway converts it to a positive number with 
+Numeric#abs and then prepends it with a negative operator to make it negative. abs returns the absolute value 
+of a number (the non-negative value of a number without regard to its sign).
+
+Thus, instead of operating by checking the value and proceeding based on the evaluation, the opposite can be 
+applied by stripping it of its sign, then giving it the negative sign.
+
+This is clearly shorter. However is it superior?
+=end
