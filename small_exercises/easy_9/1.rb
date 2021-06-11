4c1aa0f5579fcc35f4917e39f71702c77cabe618
mskalisz <mskalisz3@gmail.com>
2021-06-10 21:02:23 -0400

    Add LS solution

--- small_exercises/easy_9/1.rb
+++ small_exercises/easy_9/1.rb
@@ -43,3 +43,23 @@ end
 
 greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
 #=> Hello, John Q Doe! Nice to have a Master Plumber around.
+
+=begin
+LS Solution
+
+def greetings(name, status)
+  "Hello, #{name.join(' ')}! Nice to have a #{status[:title]} #{status[:occupation]} around."
+end
+
+Discussion
+We have two variables here, an Array and a Hash. We can use Array#join on the Array, and supply it with a ' ' 
+to change the Array into a full name with appropriate spacing. For the Hash, we simply access the items by 
+their keys.
+
+Finally we use string interpolation to combine everything into a single string and allow the method to return 
+it automatically.
+
+Further Exploration
+This method doesn't actually pass rubocop! What is the best way to shorten the lines in this method so it 
+doesn't exceed the 80 maximum characters to a line?
+=end
