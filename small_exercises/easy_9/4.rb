48caa75b352f595a3cbd83e567762c56ff13d3eb
mskalisz <mskalisz3@gmail.com>
2021-06-12 08:17:00 -0400

    Add LS solution exercise 4

--- small_exercises/easy_9/4.rb
+++ small_exercises/easy_9/4.rb
@@ -40,3 +40,19 @@ end
 p sequence(5) == [1, 2, 3, 4, 5]
 p sequence(3) == [1, 2, 3]
 p sequence(1) == [1]
+
+=begin
+LS Solution
+
+def sequence(number)
+  (1..number).to_a
+end
+
+Discussion
+This simply takes advantage of Range combined with to_a which creates a range from 1 up to the value of the 
+supplied parameter number and then converts it to an array.
+
+Further Exploration
+Food for thought: what do you think sequence should return if the argument is not greater than 0. For instance, 
+what should you do if the argument is -1? Can you alter your method to handle this case?
+=end
