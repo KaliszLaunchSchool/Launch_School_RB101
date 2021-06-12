8e70f7e5a3926ba8b213177bb717511520a1cc5e
mskalisz <mskalisz3@gmail.com>
2021-06-12 08:57:21 -0400

    Solve exercise 6.rb

---
+++ small_exercises/easy_9/6.rb
@@ -0,0 +1,81 @@
+=begin
+How long are you?
+Write a method that takes a string as an argument, and returns an Array that contains every word from the 
+string, to which you have appended a space and the word length.
+
+You may assume that words in the string are separated by exactly one space, and that any substring of non-space 
+characters is a word.
+
+Examples
+word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
+
+word_lengths("baseball hot dogs and apple pie") ==
+  ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+word_lengths("Supercalifragilisticexpialidocious") ==
+  ["Supercalifragilisticexpialidocious 34"]
+
+word_lengths("") == []
+
+# Problem
+- Create a method which takes 1 parameter (string)
+- Iterate through the array
+  - For each word, append a space and the word length
+  - Return the word with their respective lengths as an array
+- Assume:
+  - Words are separated by exacttly one space
+  - Any substring of non-space characters is a word
+
+Input: string
+Output: array
+
+# Examples
+word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
+
+word_lengths("baseball hot dogs and apple pie") ==
+  ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+word_lengths("Supercalifragilisticexpialidocious") ==
+  ["Supercalifragilisticexpialidocious 34"]
+
+word_lengths("") == []
+
+# Data
+- String
+- Array
+- Integers
+
+# Algo
+- Create a method which accepts 1 parameter (string)
+- Initiate a results array
+- Split the string at the spaces to get an array of words
+- Iterate through the array of words
+  - Add a space, and the word's size to each
+- Return the aarray
+
+# Code
+=end
+
+def word_lengths(phrase)
+  results =[]
+  phrase.split(" ").each do |word|
+    results << word + ' ' + word.size.to_s
+  end
+  results
+end
+
+p word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
+
+p word_lengths("baseball hot dogs and apple pie") ==
+  ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+p word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+p word_lengths("Supercalifragilisticexpialidocious") ==
+  ["Supercalifragilisticexpialidocious 34"]
+
+p word_lengths("") == []
