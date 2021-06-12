7c07f612ff8395cbfded98bf37b794961135091e
mskalisz <mskalisz3@gmail.com>
2021-06-12 09:24:42 -0400

    Add LS Solution exercise 7

--- small_exercises/easy_9/7.rb
+++ small_exercises/easy_9/7.rb
@@ -37,3 +37,18 @@ def swap_name(name)
 end
 
 p swap_name('Joe Roberts') == 'Roberts, Joe'
+
+=begin
+LS Solution
+
+def swap_name(name)
+  name.split(' ').reverse.join(', ')
+end
+
+Discussion
+The method is given a full name as a string. Since we know the name is divided by a space we can use 
+String#split with an argument of (' '). This gives us an array ['Joe', 'Roberts'].
+
+We then use Array#reverse to give us ['Roberts', 'Joe'] and, finally, we use Array#join with the comma and the 
+space (', ') giving us the desired result.
+=end
