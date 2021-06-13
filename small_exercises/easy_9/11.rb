32df92d5077f0f21c8962dfbb8ff7efd2eb14c96
mskalisz <mskalisz3@gmail.com>
2021-06-13 17:06:04 -0400

    Add LS solution exercise 11

--- small_exercises/easy_9/11.rb
+++ small_exercises/easy_9/11.rb
@@ -69,3 +69,42 @@ def sort_anagrams(words)
 end
 
 p sort_anagrams(words)
+
+=begin
+LS Solution
+
+result = {}
+
+words.each do |word|
+  key = word.split('').sort.join
+  if result.has_key?(key)
+    result[key].push(word)
+  else
+    result[key] = [word]
+  end
+end
+
+result.each_value do |v|
+  puts "------"
+  p v
+end
+
+Discussion
+The trickiest part of this problem is figuring out how to check if two words are anagrams. Since anagrams are 
+two words which contain exact same letters, but in different order, if we sort each anagram word, values should 
+be the same. For example:
+
+dome and mode are anagrams because when we sort both of them we get demo.
+
+Now that we know how to find anagrams, we can use a hash to store the sorted version of the word as the key in 
+the hash and its value will be an array of all anagrams of that word.
+
+Translating this to code...we first initialize a hash result.
+
+Then, we iterate over the words array and in each iteration:
+
+We check if the sorted version of the word is already in the hash. If it is we add it to the value array.
+If the sorted version is not in the hash we add it to the hash and the initial value of that key is just the 
+array with that word in it.
+In the end we print the values in the result hash.
+=end
