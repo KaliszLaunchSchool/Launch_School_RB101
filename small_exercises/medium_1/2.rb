22b5c799a3b7194c566fea9bc3f806375c656522
mskalisz <mskalisz3@gmail.com>
2021-06-13 20:02:16 -0400

    Add LS solution exercise 2

--- small_exercises/medium_1/2.rb
+++ small_exercises/medium_1/2.rb
@@ -60,3 +60,36 @@ p rotate_rightmost_digits(735291, 3) == 735912
 p rotate_rightmost_digits(735291, 4) == 732915
 p rotate_rightmost_digits(735291, 5) == 752913
 p rotate_rightmost_digits(735291, 6) == 352917
+
+=begin
+LS Solution
+
+def rotate_rightmost_digits(number, n)
+  all_digits = number.to_s.chars
+  all_digits[-n..-1] = rotate_array(all_digits[-n..-1])
+  all_digits.join.to_i
+end
+
+Discussion
+Don't be alarmed if your solution is considerably longer than our solution, so long as it produces the correct 
+results.
+
+Since we will be working with rotate_array, we need an array. To do this, we simply convert the number to a 
+string, and then split it out into its individual digits. That's the easy part.
+
+Now the tricky part. First, there's this:
+
+all_digits[-n..-1]
+
+This construct returns the last n elements of the all_digits array. It's a handy shorthand to know about, as 
+you're likely to be using it more than you might think.
+
+We pass all_digits[-n..-1] to our rotate_array method from the previous exercise, and that method returns a new 
+array with the digits rotated as needed.
+
+We then assign the return value of rotate_array to all_digits[-n..-1]. When an expression like this appears on 
+the left side of an assignment, it means, "replace the last n elements with the values to the right of the 
+equal sign". And that's exactly what happens here: we replace the last n digits with the rotated digits.
+
+Finally, we convert the all_digits array back to an integer.
+=end
