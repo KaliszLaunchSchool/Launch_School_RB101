4b3c3009c1e90a2b6e1d052ff4c82b00935fa155
mskalisz <mskalisz3@gmail.com>
2021-06-19 12:31:50 -0400

    Add LS solution exercise 2

--- small_exercises/medium_2/2.rb
+++ small_exercises/medium_2/2.rb
@@ -44,12 +44,12 @@ block_word?('jest') == true
 BLOCKED_ALPHABET = [
                     ['B','O'], ['X','K'], ['D','Q'], ['C','P' ], ['N','A'], ['G','T'], ['R','E'], 
                     ['F','S'], ['J','W'], ['H','U'], ['V','I'], ['L','Y'], ['Z','M']
-                    ]
+].freeze 
                   
 def block_word?(string)
   letters = string.upcase.split(//)
   offending_letters = []
-  
+
   BLOCKED_ALPHABET.each do |letter_blocks|
     if letters.include?(letter_blocks[0]) && letters.include?(letter_blocks[1])
       offending_letters << letter_blocks
@@ -66,3 +66,35 @@ end
 p block_word?('BATCH') == true
 p block_word?('BUTCH') == false
 p block_word?('jest') == true
+
+=begin
+Solution
+
+BLOCKS = %w(BO XK DQ CP NA GT RE FS JW HU VI LY ZM).freeze
+
+def block_word?(string)
+  up_string = string.upcase
+  BLOCKS.none? { |block| up_string.count(block) >= 2 }
+end
+
+Discussion
+The first thing we want to consider is how we should organize our blocks; we want to be able to check each 
+block easily to make sure that a valid block word is passed to this method.
+
+For this solution, we'll use an array of two letter strings. We'll use this array to check that the word passed 
+in doesn't contain two letters from any block. We also want to make sure that no block is used more than once. 
+If both those conditions are met, then we have a valid block word.
+
+In the solution the String#count method is used. This allows us to count the number of occurrences of a 
+collection of characters. The collection of characters will be each block we want to check. If we find a count 
+of 2 or greater, then we know that either both block characters are contained within the string, or that a 
+character from the current block occurs more than once in that string.
+
+The final item of note is that we must convert the input string to uppercase, just in case it contains 
+lowercase letters.
+
+Further Exploration
+Did you use a different data structure for organizing your blocks? Were those blocks 2 letter strings or 
+something else? What method did you use to check if a string had 2 letters from a single block? There are 
+several different possibilities for solving this exercise.
+=end
