b4cb87c9719a9d7bc6fc4c0ef7e9653435c0af65
mskalisz <mskalisz3@gmail.com>
2021-07-30 08:54:41 -0400

    Solve easy 1, exercise 2

--- small_exercises/small_ex_review/easy_1_2.rb
+++ small_exercises/small_ex_review/easy_1_2.rb
@@ -6,11 +6,38 @@ argument is a valid integer value.
 
 Examples:
 
-Copy Code
 puts is_odd?(2)    # => false
 puts is_odd?(5)    # => true
 puts is_odd?(-17)  # => true
 puts is_odd?(-8)   # => false
 puts is_odd?(0)    # => false
 puts is_odd?(7)    # => true
+
+#  Problem 
+- Write a method which takes 1 integer
+- Return true if the number's absolute value is odd
+- Assume valid integer
+
+# Data: Integer, boolean
+
+# Algo
+- Create a method which takes 1 parameter
+- if the number is negative, multiply by -1 before evaluation
+- if the number is odd, return true
+- if the number is even or 0, return false
 =end
+
+def is_odd?(integer)
+  if integer % 2 == 0
+    return false
+  else 
+    return true
+  end
+end
+
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
