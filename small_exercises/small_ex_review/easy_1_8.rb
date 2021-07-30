3643f8247dbaa6c5007db51798a661d1b7102cbd
mskalisz <mskalisz3@gmail.com>
2021-07-30 12:31:50 -0400

    Solve easy 1 exercise 8

--- small_exercises/small_ex_review/easy_1_8.rb
+++ small_exercises/small_ex_review/easy_1_8.rb
@@ -10,4 +10,30 @@ puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
 puts average([1, 5, 87, 45, 8, 8]) == 25
 puts average([9, 47, 23, 95, 16, 52]) == 40
 The tests above should print true.
+
+# Problem
+- Create a method which takes an array of itegers
+- Returns the averafe of all numbers
+Rules: attay will not be empty, numbers will all be positive
+
+Input: array
+Output: integer
+
+# Algo
+- Create a method which takes 1 parameter (array)
+- Sum the array
+- Divide the sum by the size of the array
+- Return the result integer
+
+# Data: 
 =end
+
+def average(array)
+  denominator = array.size
+  numerator = array.sum
+  result = numerator/denominator
+end
+
+puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
+puts average([1, 5, 87, 45, 8, 8]) == 25
+puts average([9, 47, 23, 95, 16, 52]) == 40
