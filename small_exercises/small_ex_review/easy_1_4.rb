ed9ba185f411e59497760d5db2cabfbcc7a4f075
mskalisz <mskalisz3@gmail.com>
2021-07-30 11:06:02 -0400

    Solve easy 1 exercise 4.rb

--- small_exercises/small_ex_review/easy_1_4.rb
+++ small_exercises/small_ex_review/easy_1_4.rb
@@ -17,4 +17,41 @@ car => 4
 truck => 3
 SUV => 1
 motorcycle => 2
+
+# Problem
+- Create a method which accepts 1 parameter (array)
+- Counts the number of occurences of each element
+- Words are case sensitive
+
+Input: array
+Output: hash
+
+# Algo
+- Create a method which accepts 1 parameter (array)
+- Initiate a results hash
+- Iterate through the array
+  - For each element, if the hash contains the element as the key, add 1 to the value
+  - If the hash does not contain that element as a key, add the element as a key with 1 as the value
+- Return the hash
+
+
 =end
+
+def count_occurrences(elements)
+  results = {}
+  elements.each do |element|
+    if results.include?(element)
+      results[element] += 1
+    else 
+      results[element] = 1
+    end
+  end
+  p results
+end
+
+vehicles = [
+  'car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'motorcycle', 'car', 'truck'
+]
+
+count_occurrences(vehicles)
