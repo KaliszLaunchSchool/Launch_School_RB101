562bc0e308f3bd030f776626a6873a25964c9f39
mskalisz <mskalisz3@gmail.com>
2021-07-30 09:10:19 -0400

    Solve easy 1 exercise 3

--- small_exercises/small_ex_review/easy_1_3.rb
+++ small_exercises/small_ex_review/easy_1_3.rb
@@ -7,4 +7,24 @@ puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
 puts digit_list(7) == [7]                     # => true
 puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
 puts digit_list(444) == [4, 4, 4]             # => true
+
+# Problem
+Create a method which takes 1 argument, returns a list of the digits in that numer
+
+Input: integer
+Output: array
+
+# Algo
+- Create a method which takes 1 parameter (integer)
+- Convert the integer into a string, split the string into chars
+- return that array
 =end
+
+def digit_list(integer)
+  integer.to_s.chars.map {|string| string.to_i}
+end
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
