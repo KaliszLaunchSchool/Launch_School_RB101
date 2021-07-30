fc4cd24afa97cb76fb7f232961f2a7c6a5547bb4
mskalisz <mskalisz3@gmail.com>
2021-07-30 12:55:47 -0400

    Solve easy 1, exercise 10

--- small_exercises/small_ex_review/easy_1_10.rb
+++ small_exercises/small_ex_review/easy_1_10.rb
@@ -9,4 +9,37 @@ Examples:
 puts calculate_bonus(2800, true) == 1400
 puts calculate_bonus(1000, false) == 0
 puts calculate_bonus(50000, true) == 25000
+
+# Problem
+- Create a method which takes 2 parameters (pos integer, boolean)
+- Calculates a bonus
+- If true, bonus = 1/2 salary, if false, bonus is 0
+
+Input: integer, boolean
+Output: integer
+
+# Algo
+- Create a method which takes 2 parameters (integer, boolean)
+- If boolean = false
+  - return 0
+- If boolean = true
+  - Divide the integer by 2, return that integer
 =end
+=begin
+def calculate_bonus(salary, boolean)
+  if boolean == false
+    bonus = 0
+  else
+    bonus = salary / 2
+  end
+  bonus 
+end
+=end
+
+def calculate_bonus(salary, boolean)
+  boolean ? salary/2 : 0 
+end
+
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
