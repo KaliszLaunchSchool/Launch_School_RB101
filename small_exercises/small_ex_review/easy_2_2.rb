feb8784fc0369247fc69d341841bf653c2a98bcf
mskalisz <mskalisz3@gmail.com>
2021-07-30 15:57:04 -0400

    Solve easy 2 ex 2

--- small_exercises/small_ex_review/easy_2_2.rb
+++ small_exercises/small_ex_review/easy_2_2.rb
@@ -14,4 +14,29 @@ Enter the length of the room in meters:
 Enter the width of the room in meters:
 7
 The area of the room is 70.0 square meters (753.47 square feet).
+
+# Problem
+- Create a program
+  - Asks the user for length/width of a room in meters
+  - Displays the area of the room 
+    -Displays in square meters and square feet
+
+# Data
+- String, integer, float
+
+# Algo
+- Create a program
+- Ask the user for length of a room in meters, store in a length variable
+- Ask the user for width of a room in meters, store in a width variable
+- Calculate square meters: (length * width)
+- Calculate square feet (1 square meter == 10.7639 square feet)
+- Output the results
 =end
+
+puts "Enter the length of the room in meters:"
+length = gets.chomp.to_f
+puts "Enter the width of the room in meters:"
+width = gets.chomp.to_f
+square_meters = length * width
+square_feet = square_meters * 10.7639
+puts "The area of the room is #{square_meters.round(2)} square meters (#{square_feet.round(2)} square feet)."
