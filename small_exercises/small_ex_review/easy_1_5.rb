bee7256a809f81ab26077defbbe81ae88b4b925e
mskalisz <mskalisz3@gmail.com>
2021-07-30 11:31:08 -0400

    Solve easy 1 exercise 5

--- small_exercises/small_ex_review/easy_1_5.rb
+++ small_exercises/small_ex_review/easy_1_5.rb
@@ -7,4 +7,26 @@ puts reverse_sentence('') == ''
 puts reverse_sentence('Hello World') == 'World Hello'
 puts reverse_sentence('Reverse these words') == 'words these Reverse'
 The tests above should print true.
+
+# Problem
+- Create a method which takes a string, 
+- reurns a new string, each word is reversed
+
+Input: string
+Output: sting
+
+# Algo
+- Create a method which accepts 1 parameter (string)
+- Split the string at the spaces, so each word is an element of an array
+- call the reverse method on the array
+- join the new array
+- return the new string
 =end
+
+def reverse_sentence(sentence)
+  sentence.split.reverse.join(' ')
+end
+
+puts reverse_sentence('') == ''
+puts reverse_sentence('Hello World') == 'World Hello'
+puts reverse_sentence('Reverse these words') == 'words these Reverse'
