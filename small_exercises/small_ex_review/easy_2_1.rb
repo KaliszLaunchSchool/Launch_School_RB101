b9d5fd3065cd8d8362988da25b918bfbc4f7f7c7
mskalisz <mskalisz3@gmail.com>
2021-07-30 15:28:56 -0400

    Solve easy 2 exercise 1

--- small_exercises/small_ex_review/easy_2_1.rb
+++ small_exercises/small_ex_review/easy_2_1.rb
@@ -7,4 +7,19 @@ Example Output
 
 Teddy is 69 years old!
 
+# Problem
+- Create a program that randomy generates and print's an age
+- Age is a random number between 20 and 200
+
+Input: none
+Output: string
+
+Algo
+- Create a method which, when run:
+- Randomly chooses an integer between 20 and 200
+- Outputs that number in a string
 =end
+
+age = (20..200).to_a.sample
+
+puts "Teddy is #{age} years old!"
