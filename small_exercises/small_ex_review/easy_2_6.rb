603abe97fa172b41865fdf5ac9bedf58c926d6b6
mskalisz <mskalisz3@gmail.com>
2021-08-01 20:08:32 -0400

    Solve easy 2 exercise 6

---
+++ small_exercises/small_ex_review/easy_2_6.rb
@@ -0,0 +1,10 @@
+=begin
+Print all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
+# Problem
+- Print from 1 to 99 with each number on a separate line
+
+
+=end
+
+puts (1..99).to_a
