78370933d7a8a93c0ba2d9f73b2b165d17ce0ecc
mskalisz <mskalisz3@gmail.com>
2021-08-03 22:06:01 -0400

    Solve easy 3 exercise 4

--- small_exercises/small_ex_review/easy_3_4.rb
+++ small_exercises/small_ex_review/easy_3_4.rb
@@ -5,4 +5,23 @@ Create a method that takes two arguments, multiplies them together, and returns 
 Example:
 
 multiply(5, 3) == 15
+
+# Problem
+- Create a method which accepts 2 arguments (integers?, could it be floats?)
+- Multiplies the number
+- Returns
+
+Input: 2 integers
+Output: integer
+
+# Algo
+- Create a method which accepts 2 integers
+- Multiply them together
+- return the result
 =end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+p multiply(5, 3) == 15
