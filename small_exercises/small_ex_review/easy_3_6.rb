4075d5bad42e6e601b83498b87c6088f63e10c94
mskalisz <mskalisz3@gmail.com>
2021-08-03 22:21:04 -0400

    Solve easy 3 exercise 6

--- small_exercises/small_ex_review/easy_3_6.rb
+++ small_exercises/small_ex_review/easy_3_6.rb
@@ -15,4 +15,40 @@ xor?(5.even?, 4.even?) == true
 xor?(5.odd?, 4.odd?) == true
 xor?(5.odd?, 4.even?) == false
 xor?(5.even?, 4.odd?) == false
+
+# Problem
+- Create a method which takes 2 arguments
+- returns true if exactly ONE of its arguments us truthy, otherwise false
+  - true,true == false
+  - true, false == true
+  - false, true == true
+  - false, false == false
+- Result should be boolean
+
+Input: 2 arguments 
+Output: boolean
+
+# Algo
+- Create a method which accepts 2 arguments
+- If arg 1 = true & arg 2 = true
+  - false
+- If arg 1 =false & aarg 2 = false
+  - false
+- else, return true
 =end
+
+def xor? (arg_1, arg_2)
+  if arg_1 == true && arg_2 == true
+    false
+  elsif arg_1 == false && arg_2 == false
+    false
+  else
+    true
+  end
+end
+
+
+p xor?(5.even?, 4.even?) == true
+p xor?(5.odd?, 4.odd?) == true
+p xor?(5.odd?, 4.even?) == false
+p xor?(5.even?, 4.odd?) == false
