44a09e6b6a1da84428a5cc41295e80e60ce5ddb3
mskalisz <mskalisz3@gmail.com>
2021-08-04 21:15:36 -0400

    Add prompt easy 3 exercise 10

---
+++ small_exercises/small_ex_review/easy_3_10.rb
@@ -0,0 +1,12 @@
+=begin
+Palindromic Numbers
+Write a method that returns true if its integer argument is palindromic, false otherwise. A palindromic number 
+reads the same forwards and backwards.
+
+Examples:
+
+palindromic_number?(34543) == true
+palindromic_number?(123210) == false
+palindromic_number?(22) == true
+palindromic_number?(5) == true
+=end
