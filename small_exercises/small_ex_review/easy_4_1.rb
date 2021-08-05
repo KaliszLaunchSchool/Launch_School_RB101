1896e072c9cd30a91f0cf2fcaceb158b5a5eb582
mskalisz <mskalisz3@gmail.com>
2021-08-05 09:09:39 -0400

    Solve easy 4 exercise 1

--- small_exercises/small_ex_review/easy_4_1.rb
+++ small_exercises/small_ex_review/easy_4_1.rb
@@ -9,4 +9,34 @@ Examples:
 short_long_short('abc', 'defgh') == "abcdefghabc"
 short_long_short('abcde', 'fgh') == "fghabcdefgh"
 short_long_short('', 'xyz') == "xyz"
+
+# Problem
+- Create a method which accepts 2 parameters
+- Determines the size of each string
+- Concatinates shorter + longer + shorter
+- Returns the result of the concatination
+
+Rules: Assume the strings are different lengths
+
+Input: 2 strings
+Output: concatinated string
+
+# Algo
+- Create a method which accepts 2 strings
+- Determine the size of each string
+  - determine shorter vs longer
+- Concatinate the strings using shorter + longer + shorter
 =end
+
+def short_long_short(string_1, string_2)
+  if string_1.size > string_2.size
+    longer, shorter = string_1, string_2
+  else
+    longer, shorter = string_2, string_1
+  end
+  shorter + longer + shorter
+end
+
+p short_long_short('abc', 'defgh') == "abcdefghabc"
+p short_long_short('abcde', 'fgh') == "fghabcdefgh"
+p short_long_short('', 'xyz') == "xyz"
