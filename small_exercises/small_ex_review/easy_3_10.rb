1f99bb90fe6bed1e6a16f130bcc5988899846bd7
mskalisz <mskalisz3@gmail.com>
2021-08-04 21:17:29 -0400

    Solve easy 3 exercise 10

--- small_exercises/small_ex_review/easy_3_10.rb
+++ small_exercises/small_ex_review/easy_3_10.rb
@@ -9,4 +9,16 @@ palindromic_number?(34543) == true
 palindromic_number?(123210) == false
 palindromic_number?(22) == true
 palindromic_number?(5) == true
+
+# Algo
+- Convert num into string, use string reverse check
 =end
+
+def palindromic_number?(num)
+  num.to_s == num.to_s.reverse
+end
+
+p palindromic_number?(34543) == true
+p palindromic_number?(123210) == false
+p palindromic_number?(22) == true
+p palindromic_number?(5) == true
