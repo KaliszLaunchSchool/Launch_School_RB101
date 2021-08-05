8b319d12008cb176d258b8d6c07f14e7be93dc15
mskalisz <mskalisz3@gmail.com>
2021-08-05 13:46:56 -0400

    Solve easy 4 exercise 4

--- small_exercises/small_ex_review/easy_4_4.rb
+++ small_exercises/small_ex_review/easy_4_4.rb
@@ -22,3 +22,37 @@ leap_year?(1) == false
 leap_year?(100) == true
 leap_year?(400) == true
 =end
+
+def leap_year?(year)
+  leap_year = false
+  if year >= 1752
+    if year % 4 == 0
+      leap_year = true
+      if year % 100 == 0
+        leap_year = false
+        if year % 400 == 0
+          leap_year = true
+        end
+      end
+    end
+  else
+    if year % 4 == 0
+      leap_year = true
+    end
+  end
+  leap_year
+end
+
+p leap_year?(2016) == true
+p leap_year?(2015) == false
+p leap_year?(2100) == false
+p leap_year?(2400) == true
+p leap_year?(240000) == true
+p leap_year?(240001) == false
+p leap_year?(2000) == true
+p leap_year?(1900) == false
+p leap_year?(1752) == true
+p leap_year?(1700) == true
+p leap_year?(1) == false
+p leap_year?(100) == true
+p leap_year?(400) == true
