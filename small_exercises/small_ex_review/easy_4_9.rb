f2f7cb5fa61215e998786310aa034c6d64188b40
mskalisz <mskalisz3@gmail.com>
2021-08-06 09:08:56 -0400

    Solve easy 4 exercise 9

--- small_exercises/small_ex_review/easy_4_9.rb
+++ small_exercises/small_ex_review/easy_4_9.rb
@@ -14,4 +14,31 @@ Examples
 integer_to_string(4321) == '4321'
 integer_to_string(0) == '0'
 integer_to_string(5000) == '5000'
+
+# Problem
+- Take an integer and convert it into a string
+
+# Algo
+- Create a hash for integer to string nums
+- Initiaate a method
+- Initiate an empty array
+- Loop
+  - Divide the integer by 10
+  - Push the remainder to the array
+  - Continue for the entire integer
+- Join the array
+- Reverse the string
+- Return the string
 =end
+
+INT_TO_STR = {0 => '0', 1 => '1', 2 => '2', 3 => '3', 4 => '4', 5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9',}
+
+def integer_to_string(integer)
+  array = []
+  array << integer
+  array.join
+end
+
+p integer_to_string(4321) == '4321'
+p integer_to_string(0) == '0'
+p integer_to_string(5000) == '5000'
