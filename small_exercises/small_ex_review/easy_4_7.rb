a4775c5d2ae97a6519702c2ae96cea63480b5716
mskalisz <mskalisz3@gmail.com>
2021-08-06 07:09:11 -0400

    Revise easy 4 exercise 7

--- small_exercises/small_ex_review/easy_4_7.rb
+++ small_exercises/small_ex_review/easy_4_7.rb
@@ -53,5 +53,5 @@ def string_to_integer(string)
   integer
 end
 
-p string_to_integer('4321') #== 4321
-p string_to_integer('570') #== 570
+p string_to_integer('4321') == 4321
+p string_to_integer('570') == 570
