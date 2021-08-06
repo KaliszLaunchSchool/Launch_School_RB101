1a3ca2ef4587c823761b178e6a77e52a1682f777
mskalisz <mskalisz3@gmail.com>
2021-08-06 09:52:44 -0400

    Solve easy 4 exercise 10

--- small_exercises/small_ex_review/easy_4_10.rb
+++ small_exercises/small_ex_review/easy_4_10.rb
@@ -13,4 +13,34 @@ Examples
 signed_integer_to_string(4321) == '+4321'
 signed_integer_to_string(-123) == '-123'
 signed_integer_to_string(0) == '0'
+
+# Problem
+- Take a negative or positive integer and convert into string
 =end
+
+def signed_integer_to_string(integer)
+  if integer == 0
+    integer_to_string(integer)
+  elsif integer.negative?
+    integer *= -1
+    string = integer_to_string(integer)
+    string.prepend('-')
+  else
+    string = integer_to_string(integer)
+    string.prepend('+')
+  end
+end
+
+def integer_to_string(integer)
+  array = []
+  loop do
+    array << integer.remainder(10)
+    integer /= 10
+    break if integer == 0 
+  end
+  array.reverse.join
+end
+
+p signed_integer_to_string(4321) == '+4321'
+p signed_integer_to_string(-123) == '-123'
+p signed_integer_to_string(0) == '0'
