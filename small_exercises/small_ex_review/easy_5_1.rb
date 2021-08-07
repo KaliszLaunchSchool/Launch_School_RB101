77c79e38ef017d9ad56f516af2be451cbfe22e95
mskalisz <mskalisz3@gmail.com>
2021-08-07 08:30:42 -0400

    Solve easy 5 exercise 1

--- small_exercises/small_ex_review/easy_5_1.rb
+++ small_exercises/small_ex_review/easy_5_1.rb
@@ -9,4 +9,34 @@ ascii_value('Four score') == 984
 ascii_value('Launch School') == 1251
 ascii_value('a') == 97
 ascii_value('') == 0
+
+# Problem
+- Create a method which determines the ASCII value of each character of the string
+- Sum the values
+
+Input: String
+Output: Integer
+
+# Algo
+- Create a metod which accepts one parameter (string)
+- Initiate a value variable, initialize to 0
+- Split the string into characters
+- Iterate through the characters, for each character, determine the ascii value and add it to the value variable
+- Return the value variable
 =end
+
+def ascii_value(string)
+  values = 0
+  string.chars.each do |char|
+    value = char.ord
+    values += value
+  end
+  values
+end
+
+
+
+p ascii_value('Four score') == 984
+p ascii_value('Launch School') == 1251
+p ascii_value('a') == 97
+p ascii_value('') == 0
