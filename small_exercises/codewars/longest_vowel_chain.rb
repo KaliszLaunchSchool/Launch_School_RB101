4f6fd7bb9ebc885a8f48850cec96c22da428a9b0
mskalisz <mskalisz3@gmail.com>
2021-08-08 20:45:51 -0400

    Add prompt for longest vowel chain

---
+++ small_exercises/codewars/longest_vowel_chain.rb
@@ -0,0 +1,14 @@
+=begin
+The vowel substrings in the word codewarriors are o,e,a,io. The longest of these has a length of 2. Given a lowercase string that 
+has alphabetic characters only (both vowels and consonants) and no spaces, return the length of the longest vowel substring. Vowels 
+are any of aeiou.
+
+solve("codewarriors") == 2
+solve("suoidea") == 3
+solve("iuuvgheaae") == 4)
+solve("ultrarevolutionariees") == 3
+solve("strengthlessnesses") == 1
+solve("cuboideonavicuare") == 2
+solve("chrononhotonthuooaos") == 5
+solve("iiihoovaeaaaoougjyaw") == 8
+=end
