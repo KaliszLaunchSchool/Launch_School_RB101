8685a0e132c9aa220e5593f481c1f71852efd869
mskalisz <mskalisz3@gmail.com>
2021-08-13 17:38:29 -0400

    Solve find the mine

--- interview_prep/codewars/find_the_mine.rb
+++ interview_prep/codewars/find_the_mine.rb
@@ -8,7 +8,40 @@ as the integer 1 in the 2D array. Areas in the 2D array that are not the mine wi
 The location returned should be an array where the first element is the row index, and the second element is the column 
 index of the bomb location (both should be 0 based). All 2D arrays passed into your function will be square (NxN), 
 and there will only be one mine in the array.
+
+# Problem
+- Create a method which takes an array of integers
+- Returns its position in array form
+
+Input: Array of integers
+Output: Array of integers
+
+# Algo
+- Create a method which accepts a nested array of integers (0's and 1's)
+- Find where the 1 is, and return its position as [outer_array_index, inner_array_index]
+  - Iterate through the first array, check each array and check to see which one contains the 1
+    - Save the index of that array
+  - Iterate through the array that has the 1
+    - Save the 1's index
+
+  - Return [outer_array_index, inner_array_index]
 =end
+
+def mineLocation(positions)
+  outer_array_index = 0
+  inner_array_index = 0
+  positions.each_with_index do |array, outer_index|
+    if array.include?(1)
+      outer_array_index = outer_index
+      array.each_with_index do |num, inner_index|
+        if num == 1
+          inner_array_index = inner_index
+        end        
+      end
+    end
+  end
+  [outer_array_index, inner_array_index]
+end
 
 p mineLocation( [ [1, 0, 0], [0, 0, 0], [0, 0, 0] ] ) == [0, 0]
 p mineLocation( [ [0, 0, 0], [0, 1, 0], [0, 0, 0] ] ) == [1, 1]
