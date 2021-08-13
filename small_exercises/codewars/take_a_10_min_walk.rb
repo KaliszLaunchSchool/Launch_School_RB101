33279ee00f0a940a3add0d06388a7102348f4b68
mskalisz <mskalisz3@gmail.com>
2021-08-13 16:46:20 -0400

    Solve take a 10 min walk

--- small_exercises/codewars/take_a_10_min_walk.rb
+++ small_exercises/codewars/take_a_10_min_walk.rb
@@ -10,7 +10,49 @@ Note: you will always receive a valid array containing a random assortment of di
 will never give you an empty array (that's not a walk, that's standing still!).
 
 #Problem
+- Create a function that returns true if your walk will take you 10 min AND will return you to your starting destination
+- each block takes 1 min to walk
+
+Input: array of strings
+Output: Boolean
+
+# Algo
+- Create a method which accepts an array of strings
+- array must contain 10 elements, else return false
+- if 10 elements check to be sure you return to your location via helper method:
+
+  - Initiate a starting location
+    - n/s tracker
+    - e/w tracker
+  - For every north, add 1 to n/s tracker
+  - For every sourh, subtract 1 to n/s tracker
+  - For every east, add 1 to e/w tracker
+  - For every west, subtract 1 to e/w tracker
+  - if tracker returns 0, return true, else return false
+
 =end
+
+def is_valid_walk(directions)
+  return false unless directions.size == 10
+  return_to_start?(directions)
+end
+
+def return_to_start?(directions)
+  starting_point = 0
+  directions.each do |direction|
+    if direction == 'n' || direction == 'e'
+      starting_point += 1
+    elsif direction == 's' || direction == 'w'
+      starting_point -= 1
+    end
+  end
+
+  if starting_point == 0
+    return true 
+  else
+    return false
+  end
+end
 
 p is_valid_walk(['n','s','n','s','n','s','n','s','n','s']) == true
 p is_valid_walk(['w','e','w','e','w','e','w','e','w','e','w','e']) == false
