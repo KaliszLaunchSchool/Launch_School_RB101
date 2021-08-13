5e5a935dcb277bd19fc2ead8371dec76abd642fd
mskalisz <mskalisz3@gmail.com>
2021-08-13 09:58:29 -0400

    PEDAC for Kebabize, begin creating method

--- small_exercises/codewars/kebabize.rb
+++ small_exercises/codewars/kebabize.rb
@@ -6,7 +6,28 @@ kebabize('camelsHave3Humps') // camels-have-humps
 Notes:
 
 the returned string should only contain lowercase letters
-=end
+
+# Problem
+- Create a method which accpets a string in camel case
+- Convert camel case to kebab case
+  - kebab case contains only lowercase letters
+  - Each word has '-' between it, without spaces
+
+Input: camelCase string
+Output: kebab case string
+
+# Algo
+- Create a method which takes a string in camelCase
+- Create an array with each word
+  - Rules: 1st word is from 0 index to the letter before the first upper case letter
+  - Every word after is from the upper case letter to the letter before the next upper case letter
+  - The last word is from the last uppercase letter to the end of the string
+- Downcase each word
+- Join the array with a '-' between each word
+=end 
+
+def kebabize(camel_case_string)
+end
 
 p kebabize('myCamelCasedString') == 'my-camel-cased-string'
 p kebabize('myCamelHas3Humps') == 'my-camel-has-humps'
