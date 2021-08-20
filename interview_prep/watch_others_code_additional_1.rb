4143e1452c819e326d757f73254927cc3210939d
mskalisz <mskalisz3@gmail.com>
2021-08-19 21:59:38 -0400

    Add prompt for watch others additional 1

---
+++ interview_prep/watch_others_code_additional_1.rb
@@ -0,0 +1,10 @@
+=begin
+Problem 1: Substring at index
+
+Write a method that will return a substring based on specified indices.
+=end
+
+p substring("honey", 0, 2) == "hon"
+p substring("honey", 1, 2) == "on"
+p substring("honey", 3, 9) == "ey"
+p substring("honey", 2) == "n"
