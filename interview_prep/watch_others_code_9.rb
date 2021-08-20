da4b9852ede621a1c533a87e449563cfd0c28273
mskalisz <mskalisz3@gmail.com>
2021-08-19 21:58:53 -0400

    Add files for all problems for watch others code

--- interview_prep/watch_others_code_9.rb
+++ interview_prep/watch_others_code_9.rb
@@ -4,6 +4,32 @@ Problem 1: Find Even Index
 You are given an array of integers. Your job is to take that array and find an index N where the sum of 
 the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that 
 would make this happen, return -1. Empty arrays are equal to 0 in this problem.
+
+# Problem
+- Given an array of intergers, find the index where the 
+sum of the integers to the left of N = the sum to the right of N
+  - does not include the number that is at the index
+- If there is no index to do that, return -1
+- If empty array, return 0
+
+Input: Array of integers
+Output: integer (an index)
+
+# Examples
+p find_even_index([]) == 0
+  # Empty arrays return 0
+p find_even_index([1, 2, 3, 4, 3, 2, 1]) == 3
+  # 1+2+2 = 3+2+1
+p find_even_index([1, 100, 50, -51, 1, 1]) == 1
+  # 1 = 50 - 51 + 1 + 1
+p find_even_index([1, 2, 3, 4, 5, 6]) == -1
+  #  None, return - 1
+p find_even_index([20, 10, 30, 10, 10, 15, 35]) == 3
+  # 20 + 10 + 30 = 10 + 15 + 35
+p find_even_index([20, 10, -80, 10, 10, 15, 35]) == 0
+p find_even_index([10, -80, 10, 10, 15, 35, 20]) == 6
+p find_even_index([-1, -2, -3, -4, -3, -2, -1]) == 3
+
 =end
 
 p find_even_index([]) == 0
