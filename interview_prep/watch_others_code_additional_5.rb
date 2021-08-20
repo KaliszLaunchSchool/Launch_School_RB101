cb47ef861643c23ce8920dd0c1ff63ff4507039b
mskalisz <mskalisz3@gmail.com>
2021-08-20 10:09:17 -0400

    Solve watch others code additional 5

--- interview_prep/watch_others_code_additional_5.rb
+++ interview_prep/watch_others_code_additional_5.rb
@@ -1,9 +1,22 @@
 # Problem 5: Interweaving Arrays
 =begin
-Write a method that combines two Arrays passed in as arguments and returns a new Array that contains all elements from both Array arguments, with the elements taken in alternation.
+Write a method that combines two Arrays passed in as arguments and returns a new Array that contains all elements 
+from both Array arguments, with the elements taken in alternation.
 
 You may assume that both input Arrays are non-empty and that they have the same number of elements.
 
+# Problem
+- Create a method which takes 2 arrays
+- Return a new array with all of the elements from both arguments
+- elements should be alternating
+
+Assume: each array has the same number of elements
+  - arrays are non-empty
+
 =end
+
+def interleave(arr1, arr2)
+  p arr1.zip(arr2).flatten
+end
 
 p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, "a", 2, "b", 3, "c"]
