1c7d29f5198bb6a1a4f87a984fc8646fdc04860d
mskalisz <mskalisz3@gmail.com>
2021-08-20 09:19:36 -0400

    Solve watch others code additional 2 in 2 ways

--- interview_prep/codewars/practice.rb
+++ interview_prep/codewars/practice.rb
@@ -1,2 +1,59 @@
+=begin
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or 
+other items. We want to create the text that should be displayed next to such an item.
 
+Implement the function likes which takes an array containing the names of people that like an item. It must return 
+the display text as shown in the examples:
 
+likes [] -- must be "no one likes this"
+likes ["Peter"] -- must be "Peter likes this"
+likes ["Jacob", "Alex"] -- must be "Jacob and Alex like this"
+likes ["Max", "John", "Mark"] -- must be "Max, John and Mark like this"
+likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others like this"
+For 4 or more names, the number  and 2 others simply increases.
+
+# Problem
+- Create a method which accepts 1 parameter (an array of names)
+- Depending upon the size of the array, output 'display text'
+
+Input: array
+Output: String "text"
+
+likes([]) == 'no one likes this'
+  - array empty --> 'no one likes this'
+likes(['Peter']) == 'Peter likes this'
+  - array size == 1 --> 'array[0] likes this'
+likes(['Jacob', 'Alex']) == 'Jacob and Alex like this'
+  - array size == 2 --> 'array[0] and array[1] like this
+likes(['Max', 'John', 'Mark']) == 'Max, John and Mark like this'
+  - array size == 3 --> 'array[0], array[1], and array[2] like this'
+likes(['Alex', 'Jacob', 'Mark', 'Max']) == 'Alex, Jacob and 2 others like this'
+  - array size > 3 == 'array[0], array[1] and array.size - 2 others like this'
+
+# Algorithm
+  - Create a method which accepts 1 parameter (an array of names)
+  - Create a case based off of the size of that array
+    - Output different text via string interpolation depending upon the case
+=end
+
+def likes(array)
+  size = array.size
+  case size 
+  when 0
+    "no one likes this"
+  when 1
+    "#{array[0]} likes this"
+  when 2
+    "#{array[0]} and #{array[1]} like this"
+  when 3
+    "#{array[0]}, #{array[1]} and #{array[2]} like this"
+  else
+    "#{array[0]}, #{array[1]} and #{size - 2} others like this"
+  end
+end
+
+p likes([]) == 'no one likes this'
+p likes(['Peter']) == 'Peter likes this'
+p likes(['Jacob', 'Alex']) == 'Jacob and Alex like this'
+p likes(['Max', 'John', 'Mark']) == 'Max, John and Mark like this'
+p likes(['Alex', 'Jacob', 'Mark', 'Max']) == 'Alex, Jacob and 2 others like this'
--- interview_prep/watch_others_code_additional_2.rb
+++ interview_prep/watch_others_code_additional_2.rb
@@ -1,5 +1,32 @@
+=begin
 #Problem 2: All Substrings
 # Write a method that finds all substrings in a string, no 1 letter words.
+
+Create a method which returns all substrings, unless the substring is 1 letter
+
+=end
+=begin
+def substrings(string)
+  subs = []
+  2.upto(string.size) do |length|
+    string.chars.each_cons(length) {|substring| subs << substring.join}
+  end
+  subs
+end
+=end
+
+def substrings(string)
+  subs = []
+  counter = 0
+  loop do
+    counter.upto(string.size - 1) do |idx|
+      subs << string[counter..idx]
+      end
+      counter += 1
+      break if counter == string.size - 1
+  end
+  p subs.select {|string| string.size > 1}
+end
 
 p substrings("band") == ['ba', 'ban', 'band', 'an', 'and', 'nd']
 p substrings("world") == ['wo', 'wor', 'worl', 'world', 'or', 'orl', 'orld', 'rl', 'rld', 'ld']
