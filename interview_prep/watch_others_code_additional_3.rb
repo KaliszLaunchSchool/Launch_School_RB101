2f62ff971cf514a28ebc6def402dd864810c580c
mskalisz <mskalisz3@gmail.com>
2021-08-19 22:00:32 -0400

    Add prompt for watch others additional 3

---
+++ interview_prep/watch_others_code_additional_3.rb
@@ -0,0 +1,7 @@
+
+#Problem 3: Palindromes
+# Write a method that will return all palindromes within a string.
+p palindromes('ppop') == ['pp', 'pop']
+
+# Write a method that finds the longest substring that is a palindrome within a string.
+p longest_palindrome("ppop") == 'pop'
