74533c56f31af5fc2ffe6ea6a6c87c909258bc24
mskalisz <mskalisz3@gmail.com>
2021-08-20 07:59:01 -0400

    Solve watch others code additional 1

--- interview_prep/watch_others_code_additional_1.rb
+++ interview_prep/watch_others_code_additional_1.rb
@@ -4,6 +4,10 @@ Problem 1: Substring at index
 Write a method that will return a substring based on specified indices.
 =end
 
+def substring(string, idx1, idx2 = idx1 )
+  string[idx1..idx2]
+end
+
 p substring("honey", 0, 2) == "hon"
 p substring("honey", 1, 2) == "on"
 p substring("honey", 3, 9) == "ey"
