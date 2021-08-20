eb770c76ad332d389b3376af8fbfe034f68f3800
mskalisz <mskalisz3@gmail.com>
2021-08-19 22:00:05 -0400

    Add prompt for watch others additional 2

---
+++ interview_prep/watch_others_code_additional_2.rb
@@ -0,0 +1,6 @@
+#Problem 2: All Substrings
+# Write a method that finds all substrings in a string, no 1 letter words.
+
+p substrings("band") == ['ba', 'ban', 'band', 'an', 'and', 'nd']
+p substrings("world") == ['wo', 'wor', 'worl', 'world', 'or', 'orl', 'orld', 'rl', 'rld', 'ld']
+p substrings("ppop") == ['pp', 'ppo', 'ppop', 'po', 'pop', 'op']
