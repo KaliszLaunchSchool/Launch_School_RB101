ad33a0ec5d96e6cf2731be1626fa7cb7e544ab25
mskalisz <mskalisz3@gmail.com>
2021-08-21 07:50:16 -0400

    Solve mexican wave

--- interview_prep/codewars/mexican_wave.rb
+++ interview_prep/codewars/mexican_wave.rb
@@ -8,7 +8,43 @@ string in an array where an uppercase letter is a person standing up.
 Rules
 1.  The input string will always be lower case but maybe empty.
 2.  If the character in the string is whitespace then pass over it as if it was an empty seat.
+
+# Problem
+- Create a method which turns a string into a "mexican wa"
+  - each letter in turn 'stands up'
+- String will always be lower case to begin
+- String may be empty
+- If whitespace, just skip and go to the next letter
+- No special chars,  nums
+
+Input: lowercase string
+Output: an array of the same string with each letter in turn capitalized
+
+# Algo
+- Create a method which accepts 1 parameter (string)
+- Initiate a results array
+- Split the string into characters
+  - Iterate through with the indexes
+  - at each index, check to see if it is a letter
+    - if so, capitalize that letter
+    - otherwise, move to the next iteration
+  - join the array back into a string to push to the results array
+- Return the results array
 =end
+
+def wave(string)
+  wave = []
+  characters = string.chars
+  characters.each_with_index do |char, index|
+    lowercase_wave = string.downcase
+    if ('a'..'z').include?(char)
+      lowercase_wave[index] = char.upcase
+      wave << lowercase_wave
+    end
+  end
+  p wave
+end
+
 p wave("hello") == ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
 p wave("codewars") == ["Codewars", "cOdewars", "coDewars", "codEwars", "codeWars", "codewArs", "codewaRs", "codewarS"]
 p wave("") == []
