983c9d79ed880a71129aafd21bf77a4056b3c9f5
mskalisz <mskalisz3@gmail.com>
2021-08-22 17:46:40 -0400

    Solve common prefix

--- interview_prep/codewars/common_prefix.rb
+++ interview_prep/codewars/common_prefix.rb
@@ -17,25 +17,32 @@ puts common_prefix(["flower", "flow", "flight"]) == "fl"
 
 # Algo
 - Create a method which accepts an array of strings
-- Take the first string, split it into characters, and iterate through it
- - If the character matches the character at the same index of the other strings, add that char to a result string and cont iterating
- - Otherwise, break, and return the result string
+    - Find the size of the smallest string (n)
+- Iterate through the array n number of times
+  - find the character at each string at index n, and push into a result array
+- iterate through the results, and keep only the subarraays where each char is the same
+- take one of each of those characters, and add it to a results string
+
 =end
 
 def common_prefix(array)
+  n = array[0].size
   letters_at_each_index = []
-  array[0].chars.each_with_index do |char, index|
-    array.each do |string|
-      if string[index] == char 
-        letters_at_each_index << [char]
-      end
+  n.times do |index|
+    letters = []
+    array.each do |word|
+      letters << word[index]
     end
+    letters_at_each_index << letters
   end
-p letters_at_each_index
+  same_letters = letters_at_each_index.select {|letters| letters.all?(letters[0])}
+  result = ''
+  same_letters.each {|letters| result << letters[0]}
+  result
 end
 
 puts common_prefix(["flower", "flow", "flight"]) == "fl"
-# puts common_prefix(["dog", "racecar", "car"]) == ""
-# puts common_prefix(["interspecies", "interstellar", "interstate"]) == "inters"
-# puts common_prefix(["throne", "dungeon"]) == ""
-# puts common_prefix(["throne", "throne"]) == "throne"
+puts common_prefix(["dog", "racecar", "car"]) == ""
+puts common_prefix(["interspecies", "interstellar", "interstate"]) == "inters"
+puts common_prefix(["throne", "dungeon"]) == ""
+puts common_prefix(["throne", "throne"]) == "throne"
