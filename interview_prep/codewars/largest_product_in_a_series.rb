4151d978c665b6e6070ef8c2881a2b47514f49dc
mskalisz <mskalisz3@gmail.com>
2021-08-22 11:31:33 -0400

    Add prompt for longest product in a series

---
+++ interview_prep/codewars/largest_product_in_a_series.rb
@@ -0,0 +1,17 @@
+=begin
+39. Largest product in a series
+(https://www.codewars.com/kata/529872bdd0f550a06b00026e/train/ruby)
+Complete the greatestProduct method so that it'll find the greatest product of five consecutive digits in the given string of digits.
+
+For example:
+
+greatestProduct("123834539327238239583") // should return 3240
+The input string always has more than five digits.
+
+
+=end
+p greatest_product("123834539327238239583") == 3240
+p greatest_product("395831238345393272382") == 3240
+p greatest_product("92494737828244222221111111532909999") == 5292
+p greatest_product("92494737828244222221111111532909999") == 5292
+p greatest_product("02494037820244202221011110532909999") == 0
