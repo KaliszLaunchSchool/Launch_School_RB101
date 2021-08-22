5c52503d7559b09e8b88cd6a1a9725864baecc84
mskalisz <mskalisz3@gmail.com>
2021-08-22 13:39:16 -0400

    Solve backspaces in str

--- interview_prep/codewars/backspaces_in_str.rb
+++ interview_prep/codewars/backspaces_in_str.rb
@@ -12,8 +12,31 @@ Examples
 "#######"       ==>  ""
 ""              ==>  ""
 
-# 
+# Problem
+- Create a method which takes a string of chars and #
+- Use # like a backspace
+- Return the new string
+
+# Algo
+- Create a method which takes a string
+- Create a results array
+- Iterate through the given string's characters
+  - If the char is a "#", pop a character off the results array
+  - If the char is anything else, add that char to the results array
+- join the results array
 =end
+
+def clean_string(string)
+  results = []
+  string.chars.each do |char|
+    if char == '#'
+      results.pop
+    else 
+      results << char
+    end
+  end
+  results.join
+end
 
 p clean_string('abc#d##c') == "ac"
 p clean_string('abc####d##c#') == ""
