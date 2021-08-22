41c984d323db2d1d71ff78d08c5603fc8551a7f7
mskalisz <mskalisz3@gmail.com>
2021-08-22 13:46:01 -0400

    Solve sort arrays ignoring case

--- interview_prep/codewars/sort_arrays_ignoring_case.rb
+++ interview_prep/codewars/sort_arrays_ignoring_case.rb
@@ -6,7 +6,22 @@ Sort the given strings in alphabetical order, case insensitive. For example:
 
 ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
 ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+# Problem
+- Given an array of strings, sort the strings in alphabeical order
+- ignore case
+
+Input: array of strings
+Output: sorted array of strings
+
+# Algo
+- Create a method which accepts an array of strings
+- Iterate through the array, downcasing each before sorting each string in alphabetical order
 =end
+
+def sortme(array)
+  p array.sort {|a, b| a.downcase <=> b.downcase}
+end
 
 p sortme(["Hello", "there", "I'm", "fine"]) == ["fine", "Hello", "I'm", "there"]
 p sortme(["C", "d", "a", "Ba", "be"]) == ["a", "Ba", "be", "C", "d"]
