1a838ef3ba202f5506a2bffe0c3dd07526ccaf48
mskalisz <mskalisz3@gmail.com>
2021-08-22 19:44:26 -0400

    Solve counting duplicates

--- interview_prep/codewars/counting_duplicates.rb
+++ interview_prep/codewars/counting_duplicates.rb
@@ -14,7 +14,33 @@ Example
 "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
 "aA11" -> 2 # 'a' and '1'
 "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+# Problem
+- Create a method which accepts 1 parameter (string)
+- Count the number of characters which appear more than once
+- Can be upper or lower case
+= Can be numbers
+
+# Algo
+- Create a method which accepts 1 parameter (string)
+- Initialzie a counter to 0
+- Split the string into unique, downcased characters
+- Iterate through the chars
+  - Count the number of times each downcased char appears in the downcased string
+  - if the string contains more than 1 of the same character, increment tthe counter
 =end
+
+def duplicate_count(string)
+  counter = 0
+  string = string.downcase
+  chars = string.chars.uniq
+  chars.each do |character|
+    if string.count(character) > 1
+      counter += 1
+    end
+  end
+  counter
+end
 
 p duplicate_count("") == 0
 p duplicate_count("abcde") == 0
