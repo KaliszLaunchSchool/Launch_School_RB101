5527a8b4bce6674446e5fbbb8697c652f5ddac22
mskalisz <mskalisz3@gmail.com>
2021-08-26 12:19:31 -0400

    Add prompt problem 4

---
+++ interview_prep/chris_warmup/upcase_last_char.rb
@@ -0,0 +1,6 @@
+=begin
+Create a method that upcases the last instance of that character, only when character that occurs 2 or more times in the string. 
+=end
+
+upcase_last_char('aaabbbccc') == 'aaAbbBccC'
+upcase_last_char('hello there human') = 'helLo therE Human'
