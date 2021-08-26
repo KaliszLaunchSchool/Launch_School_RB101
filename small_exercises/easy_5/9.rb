f67adad466332d648ee070fd1bba907d787f5348
mskalisz <mskalisz3@gmail.com>
2021-08-26 07:52:23 -0400

    Add additional solution for 9.rb

--- small_exercises/easy_5/9.rb
+++ small_exercises/easy_5/9.rb
@@ -56,6 +56,17 @@ p crunch('ggggggggggggggg') == 'g'
 p crunch('a') == 'a'
 p crunch('') == ''
 
+
+def crunch(words)
+  words.squeeze {|char| char}
+end
+
+p crunch('ddaaiillyy ddoouubbllee') == 'daily double'
+p crunch('4444abcabccba') == '4abcabcba'
+p crunch('ggggggggggggggg') == 'g'
+p crunch('a') == 'a'
+p crunch('') == ''
+
 =begin
 LS Solution
 
