4f69efc5c384a74247071c8501d56dc5be453f45
mskalisz <mskalisz3@gmail.com>
2021-08-26 12:17:39 -0400

    Add prompt for problem 2

---
+++ interview_prep/chris_warmup/minimum_number.rb
@@ -0,0 +1,35 @@
+=begin
+Given a List [] of n integers , find the minimum number to be inserted in a list, so that the sum of all elements of the list should equal 
+the closest prime number .
+
+Notes
+
+List size is at least 2 .
+
+List's numbers will only have positives (n > 0) .
+
+Repetition of numbers in the list could occur .
+
+The newer list's sum should equal the closest prime number .
+
+Input >> Output Examples
+
+minimum_number([3,1,2]) ==> return (1)
+Explanation:
+Since , the sum of the list's elements equal to (6) , the minimum number to be inserted to transform the sum to prime number is (1) , which 
+will make the sum of the List* equal the closest prime number (7)* .
+
+minimum_number([2,12,8,4,6]) ==> return (b, 5) Explanation:
+Since , the sum of the list's elements equal to (32) , the minimum number to be inserted to transform the sum to prime number is (5) , which 
+will make the sum of the List* equal the closest prime number (37)* .
+
+minimum_number ([50,39,49,6,17,28]) ==> return (2) Explanation:
+Since , the sum of the list's elements equal to (189) , the minimum number to be inserted to transform the sum to prime number is (2) , which 
+will make the sum of the List* equal the closest prime number (191)* .
+=end
+
+p minimum_number([3,1,2]) == 1
+p minimum_number([5,2]) == 0
+p minimum_number([1,1,1]) == 0
+p minimum_number([2,12,8,4,6]) == 5
+p minimum_number([50,39,49,6,17,28]) == 2
