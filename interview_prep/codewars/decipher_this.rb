bd8eb9e0f448133ea4ba96164ae68ea50475cbfd
mskalisz <mskalisz3@gmail.com>
2021-12-12 16:21:00 -0500

    Solve decipher this

--- interview_prep/codewars/decipher_this.rb
+++ interview_prep/codewars/decipher_this.rb
@@ -12,4 +12,58 @@ Examples
 
 decipherThis('72olle 103doo 100ya'); // 'Hello good day'
 decipherThis('82yade 115te 103o'); // 'Ready set go'
+
+# Problem
+- Given a string, find the character code which corresponds to tthe number at the 
+beginning of each word. switch the 2nd letter and the last letter
+
+# Example
+'72olle 103doo 100ya' ==> 'Hello good day'
+'82yade 115te 103o' ==> 'Ready set go'
+
+# Data: String, array
+
+# Algo
+- Split the string into an array of words
+- Iterate through each word, find the number at the beginning of each word
+- Replace the number with its corresponding character
+- Swap the 2nd letter and the last letter
+- Return that string
+
 =end
+
+ASCII = {
+  'A': 65, 'B': 66, 'C': 67, 'D': 68, 'E': 69, 'F':70, 'G': 71, 
+  'H': 72, 'I': 73, 'J': 74, 'K': 75, 'L': 76, 'M': 77, 'N': 78,
+  'O': 79, 'P': 80, 'Q': 81, 'R': 82, 'S': 83, 'T': 84, 'U': 85,
+  'V': 86, 'W': 87, 'X': 88, 'Y': 89, 'Z': 90, 'a': 97, 'b': 98,
+  'c': 99, 'd': 100, 'e': 101, 'f': 102, 'g': 103, 'h': 104, 
+  'i': 105, 'j': 106, 'k': 107, 'l': 108, 'm': 109, 'n': 110, 
+  'o': 111, 'p': 112, 'q': 113, 'r': 114, 's': 115, 't': 116, 
+  'u': 117, 'v': 118, 'w': 119, 'x': 120, 'y': 121, 'z': 122
+  }
+
+NUMS = ('0'..'9').to_a
+
+def decipher_this(sentence)
+  new_sentence = []
+  sentence.split.each do |word|
+    number = []
+    word = word.chars
+    word.each do |char|
+      number << char if NUMS.include?(char)
+    end
+    first_letter = ASCII.key(number.join.to_i).to_s
+    loop do
+      break unless NUMS.include?(word[0])
+      word.shift
+    end
+    word.unshift(first_letter)
+    word[1], word[-1] = word[-1], word[1]
+    new_sentence << word.join
+  end
+  new_sentence.join(' ')
+end
+
+p decipher_this("72olle 103doo 100ya")
+p decipher_this("82yade 115te 103o")
