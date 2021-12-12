c7397b56c529a2cacafe36509010639c7f386f17
mskalisz <mskalisz3@gmail.com>
2021-12-12 18:15:12 -0500

    Solve integer reduction

--- interview_prep/codewars/integer_reduction.rb
+++ interview_prep/codewars/integer_reduction.rb
@@ -1,5 +1,5 @@
 =begin
-In this Kata, you will be given two integers n and k and your task is to remove k-digits from n 
+Will be given two integers n and k and your task is to remove k-digits from n 
 and return the lowest number possible, without changing the order of the digits in n. Return the 
 result as a string.
 
@@ -9,7 +9,32 @@ lowest possible number. The best digits to remove are (1,2,3,6) so that the rema
 
 Note also that the order of the numbers in n does not change: solve(1284569,2) = '12456', because 
 we have removed 8 and 9.
+
+# Problem
+- Given 2 integers (n, k), remove k digits from m 
+- return the lowest possible number (as a string)
+- DO NOT change the order of digits in n
+
+# Example
+123056,4 ==> '05'
+
+# Data: array, string, integer
+
+# Algo
+- Given an integer, number
+- Create all combinations of the integer of length integer.length - number
+- return the lowest number as a string
 =end
+
+def solve(integer, number)
+  new_length = integer.to_s.length - number
+  numbers = []
+  integer.to_s.chars.combination(new_length) do |num|
+    numbers << num
+  end
+  numbers = numbers.map {|num| num.join}
+  lowest_num = numbers.min.to_s
+end
 
 solve(123056,1) #'12056')
 solve(123056,2) #'1056')
@@ -18,4 +43,4 @@ solve(123056,4) #'05')
 solve(1284569,1) #'124569')
 solve(1284569,2) #'12456')
 solve(1284569,3) #'1245')
-solve(1284569,4) #'124')s
+solve(1284569,4) #'124')
